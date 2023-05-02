5ce4c63504bb0041fa332d10acda8b5eb47cdb81
Raphaël CHARTIER <raphael.chartier@michelin.com>
2023-05-02 09:16:23 +0200

    remove vim illuminate

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -29,6 +29,7 @@ return require('lazy').setup({
             'hrsh7th/nvim-cmp',
             'hrsh7th/cmp-buffer',
             'hrsh7th/cmp-path',
+            'hrsh7th/cmp-cmdline',
             'saadparwaiz1/cmp_luasnip',
             'hrsh7th/cmp-nvim-lsp',
             'hrsh7th/cmp-nvim-lua',
@@ -94,7 +95,6 @@ return require('lazy').setup({
             "nvim-tree/nvim-web-devicons", -- optional dependency
         },
     },
-    { "RRethy/vim-illuminate" },
     { 'uga-rosa/ccc.nvim' },
     {
         'renerocksai/telekasten.nvim',
