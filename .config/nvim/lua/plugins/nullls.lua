f98e4bdf0adffbadf32afbf4f35c60f21e5b0de4
Raphaël CHARTIER <raphaelchartier@free.fr>
2023-06-21 22:58:55 +0200

    remove autoflake and autopep

--- .config/nvim/lua/plugins/nullls.lua
+++ .config/nvim/lua/plugins/nullls.lua
@@ -41,11 +41,8 @@ function M.config()
             null_ls.builtins.formatting.prettierd,
             null_ls.builtins.formatting.shfmt,
             null_ls.builtins.formatting.fixjson,
-            null_ls.builtins.formatting.autoflake,
-            null_ls.builtins.formatting.autopep8,
             null_ls.builtins.formatting.black,
             null_ls.builtins.formatting.ruff,
-            null_ls.builtins.formatting.isort,
             with_root_file(null_ls.builtins.formatting.stylua, "stylua.toml"),
 
             -- diagnostics
