bba339778d7cfc4eba801738b363a9ee83ea0da0
Raphaël CHARTIER <raphaelchartier@free.fr>
2023-06-23 17:35:55 +0200

    nvim: remove barbecue, git blame at end of line, and indent char

--- .config/nvim/lua/plugins/barbecue.lua
+++ .config/nvim/lua/plugins/barbecue.lua
@@ -1,24 +1,24 @@
 local M = {
     "utilyre/barbecue.nvim",
-    enabled = true,
     name = "barbecue",
     version = "*",
     dependencies = {
         "SmiteshP/nvim-navic",
         "nvim-tree/nvim-web-devicons", -- optional dependency
     },
+    enabled = false,
 }
 
 function M.config()
-    require('barbecue').setup({
+    require("barbecue").setup({
         theme = "catppuccin",
         integrations = {
             barbecue = {
                 dim_dirname = true,
                 bold_basename = true,
                 dim_context = false,
-            }
-        }
+            },
+        },
     })
 end
 
