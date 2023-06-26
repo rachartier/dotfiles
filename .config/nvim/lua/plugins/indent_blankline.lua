05f8a9af9f13a8182782083cb79a4e7b2a606086
Raphaël CHARTIER <raphaelchartier@free.fr>
2023-06-26 19:19:03 +0200

    nvim: change indent line color

--- .config/nvim/lua/plugins/indent_blankline.lua
+++ .config/nvim/lua/plugins/indent_blankline.lua
@@ -1,16 +1,17 @@
 local M = {
-	"lukas-reineke/indent-blankline.nvim",
+    "lukas-reineke/indent-blankline.nvim",
 }
 
 function M.config()
-	require("indent_blankline").setup({
-		char = "",
-		show_current_context = true,
-		show_current_context_start = false,
-		show_trailing_blankline_indent = false,
-		space_char_blankline = " ",
-		max_indent_increase = 1,
-	})
+    require("indent_blankline").setup({
+        char = "",
+        show_current_context = true,
+        show_current_context_start = false,
+        show_trailing_blankline_indent = false,
+        space_char_blankline = " ",
+        max_indent_increase = 1,
+    })
+    vim.api.nvim_set_hl(0, "IndentBlanklineContextChar", { link = "Comment" })
 end
 
 return M
