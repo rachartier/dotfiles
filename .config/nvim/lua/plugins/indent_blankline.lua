4fa541bda204be0792da49751e316113921e893a
Raphaël CHARTIER <raphaelchartier@free.fr>
2023-06-26 16:57:19 +0200

    nvim: fix indent blankline

--- .config/nvim/lua/plugins/indent_blankline.lua
+++ .config/nvim/lua/plugins/indent_blankline.lua
@@ -1,14 +1,16 @@
 local M = {
-    "lukas-reineke/indent-blankline.nvim",
+	"lukas-reineke/indent-blankline.nvim",
 }
 
 function M.config()
-    require("indent_blankline").setup({
-        char = "",
-        show_current_context = true,
-        show_current_context_start = false,
-        space_char_blankline = " ",
-    })
+	require("indent_blankline").setup({
+		char = "",
+		show_current_context = true,
+		show_current_context_start = false,
+		show_trailing_blankline_indent = false,
+		space_char_blankline = " ",
+		max_indent_increase = 1,
+	})
 end
 
 return M
