b239229daa02adcd15bc18faad9cb948d98e5c69
Raphaël CHARTIER <raphaelchartier@free.fr>
2023-06-29 10:33:51 +0200

    nvim: fix base16 theme

--- .config/nvim/lua/plugins/todocomments.lua
+++ .config/nvim/lua/plugins/todocomments.lua
@@ -5,7 +5,7 @@ local M = {
 function M.config()
     require("todo-comments").setup({})
 
-    vim.keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Toggle list TODO" })
+    vim.keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Toggle TODO list" })
 end
 
 return M
