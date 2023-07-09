d1944821f3f5f159fecc5cdde8a0546d4d84d1eb
Raphaël CHARTIER <raphaelchartier@free.fr>
2023-07-10 00:25:01 +0200

    nvim: readd lspsignature

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -44,6 +44,9 @@ function M.config()
                 ["vim.lsp.util.stylize_markdown"] = true,
                 ["cmp.entry.get_documentation"] = true,
             },
+            signature = {
+                enabled = false,
+            },
         },
         -- you can enable a preset for easier configuration
         presets = {
