acb13374af551e4c41740bd61e59c39681d8e612
Raphaël CHARTIER <raphaelchartier@free.fr>
2023-07-10 00:25:01 +0200

    nvim: readd lspsignature

--- .config/nvim/lua/plugins/lspsignature.lua
+++ .config/nvim/lua/plugins/lspsignature.lua
@@ -1,11 +1,19 @@
 local M = {
-    'ray-x/lsp_signature.nvim',
-    enabled = false,
+    "ray-x/lsp_signature.nvim",
+    enabled = true,
 }
 
 function M.config()
-    require "lsp_signature".setup({
+    require("lsp_signature").setup({
         noice = true,
+        hint_prefix = "🔍 ",
+        hi_parameter = "LspSignatureActiveParameter", -- how your parameter will be highlight
+        handler_opts = {
+            border = "rounded",                 -- double, rounded, single, shadow, none, or a table of borders
+        },
+
+        bind = true, -- This is mandatory, otherwise border config won't get registered.
+        -- If you want to hook lspsaga or other signature handler, pls set to false
     })
 end
 
