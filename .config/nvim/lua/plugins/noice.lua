c51adf08f8f79c41c54851cf71cfc57f76faace0
Raphaël CHARTIER <raphaelchartier@free.fr>
2023-07-09 16:58:06 +0200

    nvim: add diagflow

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -9,13 +9,23 @@ local M = {
 function M.config()
     local u = require("utils")
 
-    require "noice".setup {
+    require("noice").setup({
         cmdline = {
             format = {
-                cmdline = { title = '', icon = '  ' },
-                lua = { title = '', icon = ' 󰢱 ' },
-                help = { title = '', icon = ' 󰋖 ' }
+                cmdline = { title = "", icon = "  " },
+                lua = { title = "", icon = " 󰢱 " },
+                help = { title = "", icon = " 󰋖 " },
             },
+        },
+        messages = {
+            -- NOTE: If you enable messages, then the cmdline is enabled automatically.
+            -- This is a current Neovim limitation.
+            enabled = true,     -- enables the Noice messages UI
+            view = "mini",      -- default view for messages
+            view_error = "notify", -- view for errors
+            view_warn = "notify", -- view for warnings
+            view_history = "messages", -- view for :messages
+            view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
         },
         views = {
             cmdline_popup = {
@@ -25,7 +35,7 @@ function M.config()
                 win_options = {
                     winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
                 },
-            }
+            },
         },
         lsp = {
             -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
@@ -40,7 +50,7 @@ function M.config()
             bottom_search = true, -- use a classic bottom cmdline for search
             command_palette = true, -- position the cmdline and popupmenu together
             long_message_to_split = true, -- long messages will be sent to a split
-            inc_rename = false, -- enables an input dialog for inc-rename.nvim
+            inc_rename = false,  -- enables an input dialog for inc-rename.nvim
             lsp_doc_border = true, -- add a border to hover docs and signature help
         },
         hover = {
@@ -49,7 +59,7 @@ function M.config()
             view = nil, -- when nil, use defaults from documentation
             opts = {}, -- merged with defaults from documentation
         },
-    }
+    })
 end
 
 return M
