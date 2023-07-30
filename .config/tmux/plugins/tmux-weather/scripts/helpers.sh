2b94f293ab5c93cb9a8bc556c8f81eb4615ae1d1
Raphaël CHARTIER <raphaelchartier@free.fr>
2023-07-30 15:27:28 +0200

    tmux: redo weather

---
+++ .config/tmux/plugins/tmux-weather/scripts/helpers.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set_tmux_option() {
+  local option="$1"
+  local value="$2"
+  tmux set-option -gq "$option" "$value"
+}
+
+get_tmux_option() {
+  local option="$1"
+  local default_value="$2"
+  local option_value="$(tmux show-option -gqv "$option")"
+  if [ -z "$option_value" ]; then
+    echo "$default_value"
+  else
+    echo "$option_value"
+  fi
+}
+
+# get files age in seconds
+get_file_age() { # $1 - cache file
+  local file_path="${1:-}"
+  local now=$(date +%s)
+  local file_modification_timestamp=$(stat -c "%Y" "$file_path" 2>/dev/null || echo 0)
+  if [ $file_modification_timestamp -ne 0 ]; then
+    echo $((now - file_modification_timestamp))
+  else
+    # return 0 if could not get cache modification time, eg. file does not exist
+    echo 0
+  fi
+}
