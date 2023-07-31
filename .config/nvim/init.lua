94873353ab9010d6908860e9928bf3497abf2347
Raphaël CHARTIER <raphaelchartier@free.fr>
2023-07-31 09:30:20 +0200

    nvim: add rounded border lazy

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,7 +14,11 @@ if not vim.loop.fs_stat(lazypath) then
     })
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("plugins")
+require("lazy").setup("plugins", {
+    ui = {
+        border = "rounded",
+    },
+})
 
 require("remap")
 require("set")
