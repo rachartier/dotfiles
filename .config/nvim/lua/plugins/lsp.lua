a67047005480a0fb7238eb60a920e0359f72b192
Raphaël CHARTIER <raphael.chartier@michelin.com>
2023-08-10 17:28:10 +0200

    nvim: better lsp rename

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -18,6 +18,57 @@ function M.config()
     local lsp = require("lsp-zero")
     local U = require("utils")
 
+    local function lsp_rename()
+        local curr_name = vim.fn.expand("<cword>")
+        local value = vim.fn.input("LSP Rename: ", curr_name)
+        local lsp_params = vim.lsp.util.make_position_params()
+
+        if not value or #value == 0 or curr_name == value then
+            return
+        end
+
+        -- request lsp rename
+        lsp_params.newName = value
+        vim.lsp.buf_request(0, "textDocument/rename", lsp_params, function(_, res, ctx, _)
+            if not res then
+                return
+            end
+
+            -- apply renames
+            local client = vim.lsp.get_client_by_id(ctx.client_id)
+            vim.lsp.util.apply_workspace_edit(res, client.offset_encoding)
+
+            -- print renames
+            local changed_files_count = 0
+            local changed_instances_count = 0
+
+            if res.documentChanges then
+                for _, changed_file in pairs(res.documentChanges) do
+                    changed_files_count = changed_files_count + 1
+                    changed_instances_count = changed_instances_count + #changed_file.edits
+                end
+            elseif res.changes then
+                for _, changed_file in pairs(res.changes) do
+                    changed_instances_count = changed_instances_count + #changed_file
+                    changed_files_count = changed_files_count + 1
+                end
+            end
+
+            -- compose the right print message
+            require("notify")(
+                string.format(
+                    "Renamed %s instance%s in %s file%s.",
+                    changed_instances_count,
+                    changed_instances_count == 1 and "" or "s",
+                    changed_files_count,
+                    changed_files_count == 1 and "" or "s"
+                )
+            )
+
+            vim.cmd("silent! wa")
+        end)
+    end
+
     lsp.preset("recommended")
 
     vim.diagnostic.config({
@@ -25,7 +76,7 @@ function M.config()
     })
 
     local on_attach = function(client, bufnr)
-        client.server_capabilities.semanticTokensProvider = nil
+        -- client.server_capabilities.semanticTokensProvider = nil
 
         local bufopts = { buffer = bufnr, remap = false }
         vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
@@ -47,7 +98,10 @@ function M.config()
         -- vim.keymap.set("n", "<leader>ca", "<cmd>CodeActionMenu<cr>", bufopts, { desc = "Open code action menu" })
         vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts, { desc = "Open code action menu" })
 
-        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts, { desc = "Rename" })
+        vim.keymap.set("n", "<leader>rn", function()
+            lsp_rename()
+        end, bufopts, { desc = "Rename current symbol" })
+
         vim.keymap.set(
             "n",
             "<leader>e",
