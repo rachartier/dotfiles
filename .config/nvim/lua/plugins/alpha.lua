3b06d3280ea63a05d6d61c80ec5512012c036b9e
Raphaël CHARTIER <raphaelchartier@free.fr>
2023-09-07 09:25:01 +0200

    nvim: neotree v3.x

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -1,77 +1,77 @@
 local M = {
-    "goolord/alpha-nvim",
-    event = "VimEnter",
+	"goolord/alpha-nvim",
+	event = "VimEnter",
 }
 
 function M.config()
-    local alpha = require("alpha")
-    local dashboard = require("alpha.themes.dashboard")
+	local alpha = require("alpha")
+	local dashboard = require("alpha.themes.dashboard")
 
-    vim.api.nvim_create_autocmd("User", {
-        pattern = "LazyVimStarted",
-        desc = "Add Alpha dashboard footer",
-        once = true,
-        callback = function()
-            local stats = require("lazy").stats()
-            local ms = math.floor(stats.startuptime * 100 + 0.5) / 100
+	vim.api.nvim_create_autocmd("User", {
+		pattern = "LazyVimStarted",
+		desc = "Add Alpha dashboard footer",
+		once = true,
+		callback = function()
+			local stats = require("lazy").stats()
+			local ms = math.floor(stats.startuptime * 100 + 0.5) / 100
 
-            dashboard.section.footer.val = {
-                " ",
-                " ",
-                "Loaded " .. stats.count .. " plugins  in " .. ms .. "ms",
-            }
-            pcall(vim.cmd.AlphaRedraw)
-        end,
-    })
+			dashboard.section.footer.val = {
+				" ",
+				" ",
+				"Loaded " .. stats.count .. " plugins  in " .. ms .. "ms",
+			}
+			pcall(vim.cmd.AlphaRedraw)
+		end,
+	})
 
-    dashboard.section.header.val = {
+	dashboard.section.header.val = {
 
-        -- [[         ,--.              ,----..                                 ____  ]],
-        -- [[       ,--.'|    ,---,.   /   /   \                 ,---,        ,'  , `.]],
-        -- [[   ,--,:  : |  ,'  .' |  /   .     :        ,---.,`--.' |     ,-+-,.' _ |]],
-        -- [[,`--.'`|  ' :,---.'   | .   /   ;.  \      /__./||   :  :  ,-+-. ;   , ||]],
-        -- [[|   :  :  | ||   |   .'.   ;   /  ` ; ,---.;  ; |:   |  ' ,--.'|'   |  ;|]],
-        -- [[:   |   \ | ::   :  |-,;   |  ; \ ; |/___/ \  | ||   :  ||   |  ,', |  ':]],
-        -- [[|   : '  '; |:   |  ;/||   :  | ; | '\   ;  \ ' |'   '  ;|   | /  | |  ||]],
-        -- [['   ' ;.    ;|   :   .'.   |  ' ' ' : \   \  \: ||   |  |'   | :  | :  |,]],
-        -- [[|   | | \   ||   |  |-,'   ;  \; /  |  ;   \  ' .'   :  ;;   . |  ; |--' ]],
-        -- [['   : |  ; .''   :  ;/| \   \  ',  /    \   \   '|   |  '|   : |  | ,    ]],
-        -- [[|   | '`--'  |   |    \  ;   :    /      \   `  ;'   :  ||   : '  |/     ]],
-        -- [['   : |      |   :   .'   \   \ .'        :   \ |;   |.' ;   | |`-'      ]],
-        -- [[;   |.'      |   | ,'      `---`           '---" '---'   |   ;/          ]],
-        -- [['---'        `----'                                      '---'           ]],
-        --
-        [[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⣤⣤⣤⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀]],
-        [[⠀⣠⡶⠒⠒⠶⣄⣠⡴⠚⠁⠀⠀⠀⠀⠀⠉⠙⠳⢦⠀⠀⠀⠀⠀⠀]],
-        [[⢠⡏⠀⠀⠀⠀⠘⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢧⡀⠀⠀⠀⠀]],
-        [[⢸⡄⠀⠀⠀⠀⠀⠀⠀⠀⠋⢱⠀⠀⢠⠉⢡⠀⠀⠀⠀⠻⡄⠀⠀⠀]],
-        [[⠀⣧⠀⠀⠀⠀⠀⠀⠀⠀⣧⣾⠄⠀⢸⣦⣾⠀⠀⠀⠀⠀⢻⡄⠀⠀]],
-        [[⠀⠘⢧⡀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠸⣿⡿⠀⠀⠀⠀⠀⠈⠳⣄⠀]],
-        [[⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠈⠁⡴⠶⡆⠉⠁⠀⠀⠀⠀⠀⠀⠀⠹⡄]],
-        [[⠀⠀⠀⢷⠀⠀⠀⠀⠀⠀⠀⠀⠐⠒⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣷]],
-        [[⠀⠀⠀⠸⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠇]],
-        [[⠀⠀⠀⣀⡿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡽⣿⡛⠁⠀]],
-        [[⠀⣠⢾⣭⠀⠈⠳⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠊⠀⢠⣝⣷⡀]],
-        [[⢠⡏⠘⠋⠀⠀⠀⠈⠑⠦⣀⠀⠀⠀⠀⠀⣀⡠⠔⠋⠀⠀⠈⠛⠃⢻]],
-        [[⠈⠷⣤⣀⣀⣀⣀⣀⣀⣀⣤⡽⠟⠛⠿⣭⣄⣀⣀⣀⣀⣀⣀⣀⣤⠞]],
-        [[⠀⠀⠀⠀⠉⠉⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠉⠉⠀⠀⠀]],
-        [[⠀⠀⠀⠀      NeoVIM          ]],
-    }
+		-- [[         ,--.              ,----..                                 ____  ]],
+		-- [[       ,--.'|    ,---,.   /   /   \                 ,---,        ,'  , `.]],
+		-- [[   ,--,:  : |  ,'  .' |  /   .     :        ,---.,`--.' |     ,-+-,.' _ |]],
+		-- [[,`--.'`|  ' :,---.'   | .   /   ;.  \      /__./||   :  :  ,-+-. ;   , ||]],
+		-- [[|   :  :  | ||   |   .'.   ;   /  ` ; ,---.;  ; |:   |  ' ,--.'|'   |  ;|]],
+		-- [[:   |   \ | ::   :  |-,;   |  ; \ ; |/___/ \  | ||   :  ||   |  ,', |  ':]],
+		-- [[|   : '  '; |:   |  ;/||   :  | ; | '\   ;  \ ' |'   '  ;|   | /  | |  ||]],
+		-- [['   ' ;.    ;|   :   .'.   |  ' ' ' : \   \  \: ||   |  |'   | :  | :  |,]],
+		-- [[|   | | \   ||   |  |-,'   ;  \; /  |  ;   \  ' .'   :  ;;   . |  ; |--' ]],
+		-- [['   : |  ; .''   :  ;/| \   \  ',  /    \   \   '|   |  '|   : |  | ,    ]],
+		-- [[|   | '`--'  |   |    \  ;   :    /      \   `  ;'   :  ||   : '  |/     ]],
+		-- [['   : |      |   :   .'   \   \ .'        :   \ |;   |.' ;   | |`-'      ]],
+		-- [[;   |.'      |   | ,'      `---`           '---" '---'   |   ;/          ]],
+		-- [['---'        `----'                                      '---'           ]],
+		--
+		[[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⣤⣤⣤⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀]],
+		[[⠀⣠⡶⠒⠒⠶⣄⣠⡴⠚⠁⠀⠀⠀⠀⠀⠉⠙⠳⢦⠀⠀⠀⠀⠀⠀]],
+		[[⢠⡏⠀⠀⠀⠀⠘⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢧⡀⠀⠀⠀⠀]],
+		[[⢸⡄⠀⠀⠀⠀⠀⠀⠀⠀⠋⢱⠀⠀⢠⠉⢡⠀⠀⠀⠀⠻⡄⠀⠀⠀]],
+		[[⠀⣧⠀⠀⠀⠀⠀⠀⠀⠀⣧⣾⠄⠀⢸⣦⣾⠀⠀⠀⠀⠀⢻⡄⠀⠀]],
+		[[⠀⠘⢧⡀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠸⣿⡿⠀⠀⠀⠀⠀⠈⠳⣄⠀]],
+		[[⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠈⠁⡴⠶⡆⠉⠁⠀⠀⠀⠀⠀⠀⠀⠹⡄]],
+		[[⠀⠀⠀⢷⠀⠀⠀⠀⠀⠀⠀⠀⠐⠒⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣷]],
+		[[⠀⠀⠀⠸⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠇]],
+		[[⠀⠀⠀⣀⡿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡽⣿⡛⠁⠀]],
+		[[⠀⣠⢾⣭⠀⠈⠳⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠊⠀⢠⣝⣷⡀]],
+		[[⢠⡏⠘⠋⠀⠀⠀⠈⠑⠦⣀⠀⠀⠀⠀⠀⣀⡠⠔⠋⠀⠀⠈⠛⠃⢻]],
+		[[⠈⠷⣤⣀⣀⣀⣀⣀⣀⣀⣤⡽⠟⠛⠿⣭⣄⣀⣀⣀⣀⣀⣀⣀⣤⠞]],
+		[[⠀⠀⠀⠀⠉⠉⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠉⠉⠀⠀⠀]],
+		[[⠀⠀⠀⠀      NeoVIM          ]],
+	}
 
-    dashboard.section.buttons.val = {
-        dashboard.button("e", "󰈔  New file", "<cmd>ene<CR>"),
-        dashboard.button("SPC f f", "󰱼  Find file"),
-        dashboard.button("SPC f g", "󰺮  Live grep"),
-        dashboard.button("c", "  Configuration", "<cmd>cd ~/.config/nvim/ | e init.lua<CR>"),
-        dashboard.button("l", "  Open Lazy", "<cmd>Lazy<CR>"),
-        dashboard.button("q", "󰩈  Quit", "<cmd>qa<CR>"),
-    }
+	dashboard.section.buttons.val = {
+		dashboard.button("e", "󰈔  New file", "<cmd>ene<CR>"),
+		dashboard.button("SPC f f", "󰱼  Find file"),
+		dashboard.button("SPC f g", "󰺮  Live grep"),
+		dashboard.button("c", "  Configuration", "<cmd>cd ~/.config/nvim/ | e init.lua<CR>"),
+		dashboard.button("l", "  Open Lazy", "<cmd>Lazy<CR>"),
+		dashboard.button("q", "󰩈  Quit", "<cmd>qa<CR>"),
+	}
 
-    dashboard.config.opts.noautocmd = true
+	dashboard.config.opts.noautocmd = true
 
-    vim.cmd([[autocmd User AlphaReady echo 'ready']])
+	vim.cmd([[autocmd User AlphaReady echo 'ready']])
 
-    alpha.setup(dashboard.config)
+	alpha.setup(dashboard.config)
 end
 
 return M
