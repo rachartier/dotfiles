4b9aa65f94d12a77b498c18afcdcbc3bfc96f8b1
Raphaël CHARTIER <raphael.chartier@michelin.com>
2023-09-07 09:25:01 +0200

    nvim: neotree v3.x

--- .config/nvim/lua/plugins/statuscol.lua
+++ .config/nvim/lua/plugins/statuscol.lua
@@ -5,34 +5,42 @@ local M = {
 function M.config()
     local builtin = require("statuscol.builtin")
     local cfg = {
-        separator = " ",     -- separator between line number and buffer text ("│" or extra " " padding)
+        separator = " ", -- separator between line number and buffer text ("│" or extra " " padding)
 
         -- Builtin line number string options for ScLn() segment
-        thousands = false,     -- or line number thousands separator string ("." / ",")
-        relculright = false,   -- whether to right-align the cursor line number with 'relativenumber' set
+        thousands = false, -- or line number thousands separator string ("." / ",")
+        relculright = false, -- whether to right-align the cursor line number with 'relativenumber' set
         -- Custom line number string options for ScLn() segment
-        lnumfunc = nil,        -- custom function called by ScLn(), should return a string
-        reeval = false,        -- whether or not the string returned by lnumfunc should be reevaluated
+        lnumfunc = nil, -- custom function called by ScLn(), should return a string
+        reeval = false, -- whether or not the string returned by lnumfunc should be reevaluated
         -- Builtin 'statuscolumn' options
-        setopt = true,        -- whether to set the 'statuscolumn', providing builtin click actions
-        order = "FSNs",        -- order of the fold, sign, line number and separator segments
+        setopt = true, -- whether to set the 'statuscolumn', providing builtin click actions
+        segments = {
+            { text = { "%C" }, click = "v:lua.ScFa" },
+            { text = { "%s" }, click = "v:lua.ScSa" },
+            {
+                text = { builtin.lnumfunc, " " },
+                condition = { true, builtin.not_empty },
+                click = "v:lua.ScLa",
+            },
+        },
         -- Click actions
-        Lnum                   = builtin.lnum_click,
-        FoldPlus               = builtin.foldplus_click,
-        FoldMinus              = builtin.foldminus_click,
-        FoldEmpty              = builtin.foldempty_click,
-        DapBreakpointRejected  = builtin.toggle_breakpoint,
-        DapBreakpoint          = builtin.toggle_breakpoint,
+        Lnum = builtin.lnum_click,
+        FoldPlus = builtin.foldplus_click,
+        FoldMinus = builtin.foldminus_click,
+        FoldEmpty = builtin.foldempty_click,
+        DapBreakpointRejected = builtin.toggle_breakpoint,
+        DapBreakpoint = builtin.toggle_breakpoint,
         DapBreakpointCondition = builtin.toggle_breakpoint,
-        DiagnosticSignError    = builtin.diagnostic_click,
-        DiagnosticSignHint     = builtin.diagnostic_click,
-        DiagnosticSignInfo     = builtin.diagnostic_click,
-        DiagnosticSignWarn     = builtin.diagnostic_click,
-        GitSignsTopdelete      = builtin.gitsigns_click,
-        GitSignsUntracked      = builtin.gitsigns_click,
-        GitSignsAdd            = builtin.gitsigns_click,
-        GitSignsChangedelete   = builtin.gitsigns_click,
-        GitSignsDelete         = builtin.gitsigns_click,
+        DiagnosticSignError = builtin.diagnostic_click,
+        DiagnosticSignHint = builtin.diagnostic_click,
+        DiagnosticSignInfo = builtin.diagnostic_click,
+        DiagnosticSignWarn = builtin.diagnostic_click,
+        GitSignsTopdelete = builtin.gitsigns_click,
+        GitSignsUntracked = builtin.gitsigns_click,
+        GitSignsAdd = builtin.gitsigns_click,
+        GitSignsChangedelete = builtin.gitsigns_click,
+        GitSignsDelete = builtin.gitsigns_click,
     }
 
     require("statuscol").setup(cfg)
--- .config/nvim/lua/plugins/text-case.lua
+++ .config/nvim/lua/plugins/text-case.lua
@@ -1,14 +1,14 @@
 local M = {
-	"johmsalas/text-case.nvim",
-	key = {
-		"ga",
-	},
+    "johmsalas/text-case.nvim",
+    key = {
+        "ga",
+    },
 }
 
 function M.config()
-	require("textcase").setup({})
-	require("telescope").load_extension("textcase")
-	vim.keymap.set({ "n", "v" }, "ga", "<cmd>TextCaseOpenTelescopeChange<CR>", { desc = "Change word case" })
+    require("textcase").setup({})
+    require("telescope").load_extension("textcase")
+    vim.keymap.set({ "n", "v" }, "ga", "<cmd>TextCaseOpenTelescope<CR>", { desc = "Change word case" })
 end
 
 return M
