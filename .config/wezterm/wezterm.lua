66c98e87c4d01cd48ba65e289771842145d6f22c
Raphaël CHARTIER <raphael.chartier@michelin.com>
2023-09-13 16:26:18 +0200

    nvim: fix theme for transparent background

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,53 +1,59 @@
 local wezterm = require("wezterm")
 
-return {
-    automatically_reload_config = true,
-    color_scheme = "Catppuccin Macchiato", -- or Macchiato, Frappe, Latte
-    default_domain = "WSL:Ubuntu",
-    font = wezterm.font("CaskaydiaCove NF", { stretch = "UltraCondensed" }),
-    font_size = 14,
-    font_rules = {
-        {
-            intensity = "Bold",
-            italic = false,
-            font = wezterm.font("CaskaydiaCove NF", { weight = "ExtraBlack", stretch = "UltraCondensed" }),
-        },
-        --
-        -- {
-        --     italic = true,
-        --     font = wezterm.font("Operator Mono Book", { weight = "Bold", italic = true }),
-        -- },
-        -- {
-        --     intensity = "Bold",
-        --     italic = true,
-        --     font = wezterm.font("Operator Mono Medium Italic", { weight = "ExtraBlack", italic = true }),
-        -- },
-    },
-    harfbuzz_features = { "calt=1", "ss01=1" },
-    line_height = 0.9,
-    enable_kitty_graphics = true,
-    allow_square_glyphs_to_overflow_width = "Always",
+local gpus = wezterm.gui.enumerate_gpus()
+local config = {}
 
-    hide_tab_bar_if_only_one_tab = true,
-    adjust_window_size_when_changing_font_size = false,
-    selection_word_boundary = " \t\n{}[]()\"'`,;:│=&!%",
-    window_padding = {
-        left = 0,
-        right = 0,
-        top = 0,
-        bottom = 0,
-    },
-    front_end = "OpenGL",
-    window_close_confirmation = "NeverPrompt",
-
-    ssh_domains = {
-        {
-            name = "local.wsl",
-            -- The hostname or address to connect to. Will be used to match settings
-            -- from your ssh config file
-            remote_address = "172.19.181.196",
-            -- The username to use on the remote host
-            username = "rachartier",
-        },
-    },
+config.automatically_reload_config = true
+config.color_scheme = "Catppuccin Macchiato" -- or Macchiato, Frappe, Latte
+config.default_domain = "WSL:Ubuntu"
+config.font = wezterm.font("CaskaydiaCove NF", { stretch = "UltraCondensed" })
+config.font_size = 14
+config.font_rules = {
+	{
+		intensity = "Bold",
+		italic = false,
+		font = wezterm.font("CaskaydiaCove NF", { weight = "Bold", stretch = "UltraCondensed" }),
+	},
+	--
+	-- {
+	--     italic = true,
+	--     font = wezterm.font("Operator Mono Book", { weight = "Bold", italic = true }),
+	-- },
+	-- {
+	--     intensity = "Bold",
+	--     italic = true,
+	--     font = wezterm.font("Operator Mono Medium Italic", { weight = "ExtraBlack", italic = true }),
+	-- },
 }
+config.harfbuzz_features = { "calt=1", "ss01=1" }
+config.line_height = 0.9
+config.enable_kitty_graphics = false
+config.allow_square_glyphs_to_overflow_width = "Always"
+
+config.hide_tab_bar_if_only_one_tab = true
+config.adjust_window_size_when_changing_font_size = false
+config.selection_word_boundary = " \t\n{}[]()\"'`,;:│=&!%"
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+config.initial_rows = 16
+config.window_background_opacity = 0.9
+config.win32_system_backdrop = "Acrylic"
+
+config.window_close_confirmation = "NeverPrompt"
+
+config.ssh_domains = {
+	{
+		name = "local.wsl",
+		-- The hostname or address to connect to. Will be used to match settings
+		-- from your ssh config file
+		remote_address = "172.19.181.196",
+		-- The username to use on the remote host
+		username = "rachartier",
+	},
+}
+
+return config
