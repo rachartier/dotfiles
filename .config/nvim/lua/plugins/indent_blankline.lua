562d7ebb03a6d6f990a03f48534ae1cf0f42909d
Raphaël CHARTIER <raphaelchartier@free.fr>
2023-09-14 09:40:17 +0200

    nvim: fix theme

--- .config/nvim/lua/plugins/indent_blankline.lua
+++ .config/nvim/lua/plugins/indent_blankline.lua
@@ -1,34 +1,36 @@
 local M = {
-	"lukas-reineke/indent-blankline.nvim",
-	dependencies = {
+    "lukas-reineke/indent-blankline.nvim",
+    dependencies = {
 
-		"echasnovski/mini.indentscope",
-	},
+        "echasnovski/mini.indentscope",
+    },
+    event = { "BufReadPre", "BufNewFile" },
 }
 
 function M.config()
-	require("indent_blankline").setup({
-		-- char = "|",
-		show_current_context = false,
-		show_current_context_start = false,
-		show_trailing_blankline_indent = true,
-		-- space_char_blankline = " ",
-		max_indent_increase = 1,
-	})
-	vim.g.indent_blankline_use_treesitter = false
-	vim.g.indent_blankline_show_trailing_blankline_indent = false
+    require("indent_blankline").setup({
+        -- char = "|",
+        show_current_context = false,
+        show_current_context_start = false,
+        show_trailing_blankline_indent = true,
+        -- space_char_blankline = " ",
+        max_indent_increase = 1,
+    })
+    vim.g.indent_blankline_use_treesitter = false
+    vim.g.indent_blankline_show_trailing_blankline_indent = false
 
-	require("mini.indentscope").setup({
-		draw = {
-			delay = 0,
-			animation = require("mini.indentscope").gen_animation.none(),
-		},
-		options = {
-			indent_at_cursor = true,
-			border = "top",
-		},
-		symbol = "│",
-	})
+    require("mini.indentscope").setup({
+        draw = {
+            delay = 0,
+            animation = require("mini.indentscope").gen_animation.none(),
+        },
+        options = {
+            indent_at_cursor = true,
+            try_as_border = true,
+            border = "top",
+        },
+        symbol = "│",
+    })
 end
 
 return M
