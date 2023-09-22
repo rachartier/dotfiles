0052ec2e263b1aa54db6479318e951d1e3944c4b
Raphaël CHARTIER <lebug63@gmail.com>
2023-09-22 21:03:21 +0200

    nvim: add ft python

---
+++ .config/nvim/ftplugin/python.lua
@@ -0,0 +1,9 @@
+vim.opt_local.expandtab = true
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
+vim.opt_local.softtabstop = 4
+
+vim.opt_local.foldmethod = "indent"
+
+vim.cmd.inoreabbrev("<buffer> true True")
+vim.cmd.inoreabbrev("<buffer> false False")
