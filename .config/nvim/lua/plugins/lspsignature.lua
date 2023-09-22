fff592ac4b4fc487e6a45d96cceeda3da7965834
Raphaël CHARTIER <raphaelchartier@free.fr>
2023-09-22 14:42:56 +0200

    nvim, tmux: some tweaks

--- .config/nvim/lua/plugins/lspsignature.lua
+++ .config/nvim/lua/plugins/lspsignature.lua
@@ -1,24 +1,23 @@
 local M = {
-    "ray-x/lsp_signature.nvim",
-    enabled = true,
+	"ray-x/lsp_signature.nvim",
+	enabled = true,
 }
 
 function M.config()
-    local U = require("utils")
+	local U = require("utils")
 
-    require("lsp_signature").setup({
-        noice = true,
-        hint_enable = false,
-        hint_prefix = "󱙇 ",
-        hi_parameter = "LspSignatureActiveParameter", -- how your parameter will be highlight
-        floating_window = false,
-        handler_opts = {
-            border = U.default_border, -- double, rounded, single, shadow, none, or a table of borders
-        },
-        bind = false,         -- This is mandatory, otherwise border config won't get registered.
-        -- If you want to hook lspsaga or other signature handler, pls set to false
-        noice = true,
-    })
+	require("lsp_signature").setup({
+		hint_enable = false,
+		hint_prefix = "󱙇 ",
+		hi_parameter = "LspSignatureActiveParameter", -- how your parameter will be highlight
+		floating_window = false,
+		handler_opts = {
+			border = U.default_border, -- double, rounded, single, shadow, none, or a table of borders
+		},
+		bind = false, -- This is mandatory, otherwise border config won't get registered.
+		-- If you want to hook lspsaga or other signature handler, pls set to false
+		noice = true,
+	})
 end
 
 return M
--- .config/nvim/lua/themes/base16/theme.lua
+++ .config/nvim/lua/themes/base16/theme.lua
@@ -1,124 +1,174 @@
 require("base16-colorscheme").with_config({
-    telescope = false,
-    telescope_borders = true,
-    indentblankline = true,
-    notify = true,
-    ts_rainbow = true,
-    cmp = true,
-    illuminate = true,
-    lsp_semantic = true,
-    mini_completion = true,
+	telescope = true,
+	telescope_borders = true,
+	indentblankline = true,
+	notify = true,
+	ts_rainbow = true,
+	cmp = true,
+	illuminate = true,
+	lsp_semantic = true,
+	mini_completion = true,
 })
 
-local base16_colors = {
-    base00 = "#1e1e28", -- or #16161D (not catppuccin)
-    base01 = "#1a1826",
-    base02 = "#302d41",
-    base03 = "#575268",
-    base04 = "#6e6c7c",
-    base05 = "#d7dae0",
-    base06 = "#f5e0dc",
-    base07 = "#c9cbff",
-    base08 = "#f28fad",
-    base09 = "#f8bd96",
-    base0A = "#fae3b0",
-    base0B = "#abe9b3",
-    base0C = "#b5e8e0",
-    base0D = "#96cdfb",
-    base0E = "#ddb6f2",
-    base0F = "#f2cdcd",
-}
+vim.cmd("colorscheme base16-catppuccin-macchiato")
 
-vim.cmd("colorscheme base16-catppuccin")
-require("base16-colorscheme").setup(base16_colors)
+local base16_colors = require("base16-colorscheme").colors
 
 local colors = {
-    surface0 = base16_colors.base02,
-    surface2 = base16_colors.base04,
-    overlay1 = base16_colors.base03,
-    overlay2 = base16_colors.base05,
-    mantle = base16_colors.base01,
-    crust = base16_colors.base00,
-    pink = base16_colors.base0E,
-    green = base16_colors.base0B,
-    red = base16_colors.base08,
-    yellow = base16_colors.base0A,
-    blue = base16_colors.base0D,
-    flamingo = base16_colors.base0F,
-    text = base16_colors.base07,
-    peach = base16_colors.base0F,
-    base = base16_colors.base00,
-    subtext1 = base16_colors.base06,
-    orange = base16_colors.base09,
+	surface0 = base16_colors.base02,
+	surface2 = base16_colors.base04,
+	overlay1 = base16_colors.base03,
+	overlay2 = base16_colors.base05,
+	mantle = base16_colors.base01,
+	crust = base16_colors.base00,
+	pink = base16_colors.base0E,
+	green = base16_colors.base0B,
+	red = base16_colors.base08,
+	yellow = base16_colors.base0A,
+	blue = base16_colors.base0D,
+	flamingo = base16_colors.base0F,
+	text = base16_colors.base07,
+	peach = base16_colors.base0F,
+	base = base16_colors.base00,
+	subtext1 = base16_colors.base06,
+	orange = base16_colors.base09,
 }
 
 local global_highlight = {
-    LineNr = {
-        fg = colors.surface2,
-    },
-    CursorLineNr = {
-        fg = colors.yellow,
-    },
-    LspSignatureActiveParameter = {
-        bg = colors.base,
-    },
-    Pmenu = {
-        link = "PopupNormal",
-    },
-    PmenuSel = {
-        bg = colors.blue,
-        fg = colors.mantle,
-        bold = true,
-    },
-    PmenuBorder = {
-        link = "PopupBorder",
-    },
-    IlluminatedWordRead = {
-        bold = true,
-        bg = colors.surface0,
-    },
-    IlluminatedWordWrite = {
-        bold = true,
-        bg = colors.surface0,
-    },
-    IluminatedReferenceText = {
-        bold = true,
-        bg = colors.surface0,
-    },
-    CmpItemAbbr = {
-        bg = colors.crust,
-    },
-    DiagnosticWarn = {
-        fg = colors.yellow,
-    },
-    DiagnosticInfo = {
-        fg = colors.blue,
-    },
+	-- Visual = { bg = "#234370" },
+	Visual = { bg = "#5B6076" },
+	-- Visual = { bg = colors.blue, fg = colors.crust },
+	LineNr = {
+		fg = colors.overlay1,
+	},
+	CursorLineNr = {
+		fg = colors.mauve,
+	},
+	NormalFloat = {
+		bg = "None",
+		fg = colors.text,
+	},
+	NoiceCmdlinePopupBorder = {
+		link = "NormalFloat",
+	},
+	NoiceFormatLevelOff = {
+		bg = colors.yellow,
+	},
+	FloatBorder = {
+		bg = "None",
+		fg = colors.text,
+	},
+	FloatTitle = {
+		fg = colors.text,
+	},
+	PopupBorder = {
+		link = "FloatBorder",
+	},
+	PopupNormal = {
+		bg = "None",
+		fg = colors.text,
+	},
+	Pmenu = {
+		link = "FloatBorder",
+	},
+	PmenuBorder = {
+		link = "FloatBorder",
+	},
+	PmenuDocBorder = {
+		link = "FloatBorder",
+	},
+	PmenuSel = {
+		-- link = "Visual",
+		bg = colors.surface0,
+	},
+	NeoTreeNormal = {
+		bg = "None",
+	},
+	IlluminatedWordRead = {
+		bold = true,
+		bg = colors.surface1,
+	},
+	IlluminatedWordWrite = {
+		link = "IlluminatedWordRead",
+	},
+	IluminatedReferenceText = {
+		link = "IlluminatedWordRead",
+	},
+	CmpItemAbbrMatch = {
+		fg = colors.blue,
+	},
+	CmpItemAbbrMatchFuzzy = {
+		fg = colors.sapphire,
+	},
+	TelescopeSelection = {
+		link = "PmenuSel",
+	},
+	FlashLabel = {
+		fg = "#000000",
+		bg = "#FFFF00",
+		bold = true,
+	},
+	NeoTreeIndentMarker = {
+		fg = colors.surface0,
+	},
+	NeoTreeCursorLine = {
+		link = "PmenuSel",
+	},
+	NoicePopupmenu = {
+		link = "PopupNormal",
+	},
+	NoicePopupmenuBorder = {
+		link = "FloatBorder",
+	},
+	MiniIndentscopeSymbol = {
+		link = "Comment",
+	},
+	LspSignatureActiveParameter = {
+		bg = colors.peach,
+		fg = colors.crust,
+	},
 }
 
 for hl, col in pairs(global_highlight) do
-    vim.api.nvim_set_hl(0, hl, col)
+	vim.api.nvim_set_hl(0, hl, col)
 end
 
 local TelescopeColor = {
-    TelescopeMatching = {
-        fg = colors.blue,
-    },
-    TelescopeSelection = { fg = colors.text, bold = true },
+	TelescopeMatching = { link = "CmpItemAbbrMatch" },
 
-    TelescopePromptPrefix = { bg = colors.surface0 },
-    TelescopePromptNormal = { bg = colors.surface0 },
-    TelescopeResultsNormal = { bg = colors.mantle, fg = colors.surface2 },
-    TelescopePreviewNormal = { bg = colors.mantle },
-    TelescopePromptBorder = { bg = colors.surface0, fg = colors.surface0 },
-    TelescopeResultsBorder = { bg = colors.mantle, fg = colors.mantle },
-    TelescopePreviewBorder = { bg = colors.mantle, fg = colors.mantle },
-    TelescopePromptTitle = { bg = colors.pink, fg = colors.mantle },
-    TelescopeResultsTitle = { fg = colors.mantle },
-    TelescopePreviewTitle = { bg = colors.green, fg = colors.mantle },
-    TelescopeBorder = { fg = colors.text },
+	-- TelescopePromptPrefix = { bg = colors.surface0 },
+	-- TelescopePromptNormal = { bg = colors.surface0 },
+	-- TelescopePromptBorder = { bg = colors.surface0, fg = colors.surface0 },
+	TelescopePromptTitle = { bg = colors.pink, fg = colors.base },
+
+	TelescopeResultsNormal = { fg = colors.subtext1 },
+	-- TelescopeResultsBorder = { bg = colors.mantle, fg = colors.mantle },
+	TelescopeResultsTitle = { bg = colors.blue, fg = colors.base },
+
+	-- TelescopePreviewBorder = { bg = colors.base, fg = colors.base },
+	TelescopePreviewNormal = {},
+	TelescopePreviewTitle = { bg = colors.green, fg = colors.base },
+
+	-- TelescopeBorder = { fg = colors.base },
 }
 
 for hl, col in pairs(TelescopeColor) do
-    vim.api.nvim_set_hl(0, hl, col)
+	vim.api.nvim_set_hl(0, hl, col)
 end
+
+vim.api.nvim_set_hl(0, "@parameter", { fg = colors.lavender, italic = false })
+vim.api.nvim_set_hl(0, "@attribute", { fg = colors.lavender, italic = true })
+vim.api.nvim_set_hl(0, "@keyword.operator", { link = "@repeat" })
+
+vim.api.nvim_set_hl(0, "@type.builtin", { fg = colors.red })
+vim.api.nvim_set_hl(0, "@type.qualifier", { fg = colors.red })
+vim.api.nvim_set_hl(0, "@storageclass", { fg = colors.red })
+vim.api.nvim_set_hl(0, "@field", { fg = colors.red })
+
+vim.api.nvim_set_hl(0, "@property", { fg = colors.text })
+vim.api.nvim_set_hl(0, "@field", { fg = colors.text })
+
+vim.api.nvim_set_hl(0, "@variable.builtin", { fg = colors.red })
+
+-- vim.api.nvim_set_hl(0, "@variable", { fg = colors.text })
+vim.api.nvim_set_hl(0, "@_parent", { fg = colors.peach })
