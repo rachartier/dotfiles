5d2c6eb7988dd711eff9b67722b5eb33bb112467
Raphaël CHARTIER <raphael.chartier@michelin.com>
2023-10-09 11:28:42 +0200

    nvim: none ls

--- .config/nvim/lua/plugins/markdown_preview.lua
+++ .config/nvim/lua/plugins/markdown_preview.lua
@@ -1,15 +1,24 @@
 local M = {
-    "ellisonleao/glow.nvim",
-    cmd = "Glow",
+	"ellisonleao/glow.nvim",
+	cmd = "Glow",
 }
 
 function M.config()
-    require("glow").setup({
-        border = "rounded",
-    })
+	require("glow").setup({
+		border = "rounded",
+	})
 end
 
 return {
-    "iamcco/markdown-preview.nvim",
-    build = "cd app && npm install",
+	M,
+	{
+		"iamcco/markdown-preview.nvim",
+		build = "cd app && npm install",
+	},
+	{
+		"richardbizik/nvim-toc",
+		config = function()
+			require("nvim-toc").setup({})
+		end,
+	},
 }
--- .config/nvim/lua/plugins/nullls.lua
+++ .config/nvim/lua/plugins/nullls.lua
@@ -1,5 +1,5 @@
 local M = {
-	"jose-elias-alvarez/null-ls.nvim",
+	"nvimtools/none-ls.nvim",
 	event = { "BufReadPre", "BufNewFile" },
 }
 
@@ -38,7 +38,7 @@ function M.config()
 		end,
 		sources = {
 			null_ls.builtins.formatting.stylua,
-			null_ls.builtins.diagnostics.eslint,
+			-- null_ls.builtins.diagnostics.eslint,
 			null_ls.builtins.completion.spell,
 			null_ls.builtins.formatting.prettierd,
 			-- null_ls.builtins.formatting.fixjson,
@@ -55,6 +55,9 @@ function M.config()
 
 			-- hover
 			null_ls.builtins.hover.dictionary,
+			null_ls.builtins.hover.printenv,
+
+			null_ls.builtins.completion.spell,
 		},
 	})
 end
