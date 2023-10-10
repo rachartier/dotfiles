12f1223d1228100d3efb995b5df035bfbd44097d
Raphaël CHARTIER <raphaelchartier@free.fr>
2023-10-10 14:52:41 +0200

    nvim: fix indent blankline

--- .config/nvim/lua/plugins/nullls.lua
+++ .config/nvim/lua/plugins/nullls.lua
@@ -46,12 +46,11 @@ function M.config()
 			null_ls.builtins.formatting.ruff,
 			null_ls.builtins.formatting.isort,
 
-			null_ls.builtins.formatting.csharpier,
+			-- null_ls.builtins.formatting.csharpier,
 
 			-- code actions
 			null_ls.builtins.code_actions.refactoring,
 			null_ls.builtins.code_actions.gitsigns,
-			null_ls.builtins.code_actions.proselint,
 
 			-- hover
 			null_ls.builtins.hover.dictionary,
