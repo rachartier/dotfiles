eac895af4d9c2077c81895a23110d8bdecab524a
Raphaël CHARTIER <raphaelchartier@free.fr>
2023-10-12 20:34:04 +0200

    nvim: redo statuscol

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -72,4 +72,19 @@ function M.buffers_clean()
     end
 end
 
+function M.dump(o)
+    if type(o) == "table" then
+        local s = "{ "
+        for k, v in pairs(o) do
+            if type(k) ~= "number" then
+                k = '"' .. k .. '"'
+            end
+            s = s .. "[" .. k .. "] = " .. M.dump(v) .. ","
+        end
+        return s .. "} "
+    else
+        return tostring(o)
+    end
+end
+
 return M
