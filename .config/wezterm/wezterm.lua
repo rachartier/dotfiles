864242b6f427fbd2170787b626b3e9f3b5c9034e
Raphaël CHARTIER <raphaelchartier@free.fr>
2023-10-12 13:54:15 +0200

    nvim: change statuscol

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -34,10 +34,10 @@ config.hide_tab_bar_if_only_one_tab = true
 config.adjust_window_size_when_changing_font_size = false
 config.selection_word_boundary = " \t\n{}[]()\"'`,;:│=&!%"
 config.window_padding = {
-	left = "1cell",
-	right = "1cell",
-	top = 0,
-	bottom = "0.15cell",
+  left = '1cell',
+  right = '1cell',
+  top = '0.5cell',
+  bottom = '0cell',
 }
 -- config.window_background_opacity = 0
 -- config.win32_system_backdrop = "Acrylic"
