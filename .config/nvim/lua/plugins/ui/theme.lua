e7f3d6b2ffb98bbd1bc269ce0121231e6148e394
Raphaël CHARTIER <raphaelchartier@free.fr>
2023-10-15 02:35:47 +0200

    nvim: fix switchbuffer terminal

---
+++ .config/nvim/lua/plugins/ui/theme.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "catppuccin/nvim",
+        priority = 1000,
+    },
+    {
+        "NTBBloodbath/sweetie.nvim",
+        priority = 1000,
+    },
+    {
+        "RRethy/nvim-base16",
+        priority = 1000,
+    },
+}
