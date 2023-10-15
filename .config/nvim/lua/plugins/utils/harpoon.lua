d769c495a519affc799b86eebfafe04394f0d245
Raphaël CHARTIER <raphaelchartier@free.fr>
2023-10-15 16:39:25 +0200

    nvim: fix bugs

--- .config/nvim/lua/plugins/utils/harpoon.lua
+++ .config/nvim/lua/plugins/utils/harpoon.lua
@@ -1,11 +1,13 @@
 local M = {
     "ThePrimeagen/harpoon",
+    enabled = false,
 }
 
 function M.config()
     local mark = require("harpoon.mark")
     local ui = require("harpoon.ui")
 
+    require("telescope").load_extension("harpoon")
     require("harpoon").setup({})
 
     vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Harpoon add mark" })
