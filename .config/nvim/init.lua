15318efaffa251487bcbcf47c041cd37632f3044
Raphaël CHARTIER <raphaelchartier@free.fr>
2023-10-17 17:41:32 +0200

    nvim: enable checker lazy

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,26 +6,26 @@ local U = require("utils")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins", {
-	ui = {
-		border = U.default_border,
-	},
-	checker = {
-		-- automatically check for plugin updates
-		enabled = false,
-		concurrency = nil, ---@type number? set to 1 to check for updates very slowly
-		frequency = 3600, -- check for updates every hour
-	},
+    ui = {
+        border = U.default_border,
+    },
+    checker = {
+        -- automatically check for plugin updates
+        enabled = true,
+        concurrency = nil, ---@type number? set to 1 to check for updates very slowly
+        frequency = 3600, -- check for updates every hour
+    },
 })
 
 require("set")
@@ -33,17 +33,17 @@ require("remap")
 require("autocmds")
 
 function R(name)
-	require("plenary.reload").reload_module(name)
+    require("plenary.reload").reload_module(name)
 end
 
 vim.keymap.set("n", "<Leader>db", function()
-	local curbufnr = vim.api.nvim_get_current_buf()
-	local buflist = vim.api.nvim_list_bufs()
-	for _, bufnr in ipairs(buflist) do
-		if vim.bo[bufnr].buflisted and bufnr ~= curbufnr and (vim.fn.getbufvar(bufnr, "bufpersist") ~= 1) then
-			vim.cmd("bd " .. tostring(bufnr))
-		end
-	end
+    local curbufnr = vim.api.nvim_get_current_buf()
+    local buflist = vim.api.nvim_list_bufs()
+    for _, bufnr in ipairs(buflist) do
+        if vim.bo[bufnr].buflisted and bufnr ~= curbufnr and (vim.fn.getbufvar(bufnr, "bufpersist") ~= 1) then
+            vim.cmd("bd " .. tostring(bufnr))
+        end
+    end
 end, { silent = true, desc = "Close unused buffers" })
 
 require("theme")
