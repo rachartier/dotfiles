84f694565806f2e1fbf38cc188e289762f1667a7
Raphaël CHARTIER <raphaelchartier@free.fr>
2023-10-23 09:31:00 +0200

    nvim: change legendary icons

--- .config/nvim/lua/plugins/ui/legendary.lua
+++ .config/nvim/lua/plugins/ui/legendary.lua
@@ -12,6 +12,16 @@ function M.config()
             lazy_nvim = { auto_register = true },
             which_key = { auto_register = false },
         },
+        icons = {
+            -- keymap items list the modes in which the keymap applies
+            -- by default, you can show an icon instead by setting this to
+            -- a non-nil icon
+            keymap = nil,
+            command = " ",
+            fn = "󰊕 ",
+            itemgroup = " ",
+        },
+        col_separator_char = "│",
     })
 end
 
