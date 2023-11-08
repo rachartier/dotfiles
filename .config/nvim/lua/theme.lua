0ed2e9ea08be8aca38b3d4c763ce843ad880252a
Raphaël CHARTIER <raphael.chartier@michelin.com>
2023-11-08 10:50:29 +0100

    nvim: better autopair

--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -1,19 +1,19 @@
 local M = {}
 
 -- M._theme = require("themes.base16.theme")
-M._theme = require("themes.nano.theme")
--- M._theme = require("themes.catppuccin.theme")
+-- M._theme = require("themes.nano.theme")
+M._theme = require("themes.catppuccin.theme")
 
 function M.setup()
-    M._theme.setup()
+	M._theme.setup()
 end
 
 function M.get_colors()
-    return M._theme.get_colors()
+	return M._theme.get_colors()
 end
 
 function M.get_lualine_colors()
-    return M._theme.get_lualine_colors()
+	return M._theme.get_lualine_colors()
 end
 
 return M
