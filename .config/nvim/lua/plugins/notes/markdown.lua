b50da0c588a1d8793ecc6e313cb4724944708a97
Raphaël CHARTIER <raphael.chartier@michelin.com>
2023-11-20 14:19:55 +0100

    nvim: fix telekasten key

--- .config/nvim/lua/plugins/notes/markdown.lua
+++ .config/nvim/lua/plugins/notes/markdown.lua
@@ -1,40 +1,40 @@
 return {
-    {
-        "ellisonleao/glow.nvim",
-        ft = "markdown",
-        config = function()
-            require("glow").setup({
-                border = "rounded",
-            })
-        end,
-    },
-    {
-        "richardbizik/nvim-toc",
-        ft = "markdown",
-        config = function()
-            require("nvim-toc").setup({})
-        end,
-    },
-    {
-        "dhruvasagar/vim-table-mode",
-        ft = "markdown",
-        config = function()
-            vim.g.table_mode_corner = "|"
-        end,
-    },
-    {
-        "lukas-reineke/headlines.nvim",
-        dependencies = "nvim-treesitter/nvim-treesitter",
-        ft = {
-            "markdown",
-            "neorg",
-        },
-        config = function()
-            require("headlines").setup({
-                markdown = {
-                    fat_headline_lower_string = "▀",
-                },
-            })
-        end,
-    },
+	{
+		"ellisonleao/glow.nvim",
+		ft = "markdown",
+		config = function()
+			require("glow").setup({
+				border = "rounded",
+			})
+		end,
+	},
+	{
+		"richardbizik/nvim-toc",
+		ft = "markdown",
+		config = function()
+			require("nvim-toc").setup({})
+		end,
+	},
+	{
+		"dhruvasagar/vim-table-mode",
+		ft = "markdown",
+		config = function()
+			vim.g.table_mode_corner = "|"
+		end,
+	},
+	-- {
+	--     "lukas-reineke/headlines.nvim",
+	--     dependencies = "nvim-treesitter/nvim-treesitter",
+	--     ft = {
+	--         "markdown",
+	--         "neorg",
+	--     },
+	--     config = function()
+	--         require("headlines").setup({
+	--             markdown = {
+	--                 fat_headline_lower_string = "▀",
+	--             },
+	--         })
+	--     end,
+	-- },
 }
