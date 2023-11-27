db7f7edd93f2f1371fb79e0231f952135027271a
Raphaël CHARTIER <raphaelchartier@free.fr>
2023-11-27 08:46:11 +0100

    nvim: remove cursorline bg

--- .config/nvim/lua/themes/groups.lua
+++ .config/nvim/lua/themes/groups.lua
@@ -1,177 +1,179 @@
 local M = {}
 
 function M.get(colors)
-    local bg = "None"
+	local bg = "None"
 
-    if vim.g.neovide then
-        bg = colors.base
-    end
+	if vim.g.neovide then
+		bg = colors.base
+	end
 
-    return {
-        -- Visual = { bg = "#234370" },
-        Visual = { bg = "#5B6076" },
-        -- Visual = { bg = colors.blue, fg = colors.crust },
-        LineNr = {
-            fg = colors.surface1,
-        },
-        CursorLineNr = {
-            fg = colors.overlay1,
-        },
-        -- CursorLine = { bg = "None" },
-        NormalFloat = {
-            bg = bg,
-            fg = colors.text,
-        },
-        NoiceCmdlinePopupBorder = {
-            link = "NormalFloat",
-        },
-        NoiceFormatLevelOff = {
-            bg = colors.yellow,
-        },
-        FloatBorder = {
-            -- bg = "None",
-            bg = bg,
-            fg = colors.text,
-        },
-        FloatTitle = {
-            fg = colors.text,
-        },
-        PopupBorder = {
-            link = "FloatBorder",
-        },
-        PopupNormal = {
-            -- bg = "None",
-            bg = bg,
-            fg = colors.text,
-        },
-        Pmenu = {
-            link = "FloatBorder",
-        },
-        PmenuBorder = {
-            link = "FloatBorder",
-        },
-        PmenuDocBorder = {
-            link = "FloatBorder",
-        },
-        PmenuSel = {
-            -- link = "Visual",
-            bg = colors.surface0,
-        },
-        NeoTreeNormal = {
-            -- bg = "None",
-            bg = bg,
-        },
-        IlluminatedWordRead = {
-            bold = true,
-            bg = colors.surface1,
-        },
-        IlluminatedWordWrite = {
-            link = "IlluminatedWordRead",
-        },
-        IluminatedReferenceText = {
-            link = "IlluminatedWordRead",
-        },
-        CmpItemAbbrMatch = {
-            fg = colors.mauve,
-            -- bold = true,
-            -- underline = true,
-        },
-        CmpItemAbbrMatchFuzzy = {
-            -- fg = colors.blue,
-            -- underline = true,
-            link = "CmpItemAbbrMatch",
-        },
-        TelescopeSelection = {
-            link = "PmenuSel",
-        },
-        FlashLabel = {
-            fg = "#000000",
-            bg = "#FFFF00",
-            bold = true,
-        },
-        NeoTreeIndentMarker = {
-            fg = colors.surface0,
-        },
-        NeoTreeCursorLine = {
-            link = "PmenuSel",
-        },
-        NoicePopupmenu = {
-            link = "PopupNormal",
-        },
-        NoicePopupmenuBorder = {
-            link = "FloatBorder",
-        },
-        MiniIndentscopeSymbol = {
-            fg = colors.surface0,
-        },
-        LspSignatureActiveParameter = {
-            bg = colors.peach,
-            fg = colors.crust,
-        },
-        GitSignsChange = {
-            fg = colors.blue,
-        },
-        GitSignsAdd = {
-            fg = colors.green,
-        },
-        SwitchBufferStatusColor = {
-            fg = colors.red,
-        },
-        CmpGhostText = { link = "Comment", default = true },
-        -- StatusLine = {
-        --     bg = colors.mantle,
-        -- },
-        --
-        -- StatusLineNC = {
-        --     link = "StatusLine",
-        -- },
+	return {
+		-- Visual = { bg = "#234370" },
+		Visual = { bg = "#5B6076" },
+		-- Visual = { bg = colors.blue, fg = colors.crust },
+		LineNr = {
+			fg = colors.surface1,
+		},
+		CursorLineNr = {
+			fg = colors.overlay1,
+		},
+		CursorLine = {
+			bg = "None",
+		},
+		NormalFloat = {
+			bg = bg,
+			fg = colors.text,
+		},
+		NoiceCmdlinePopupBorder = {
+			link = "NormalFloat",
+		},
+		NoiceFormatLevelOff = {
+			bg = colors.yellow,
+		},
+		FloatBorder = {
+			-- bg = "None",
+			bg = bg,
+			fg = colors.text,
+		},
+		FloatTitle = {
+			fg = colors.text,
+		},
+		PopupBorder = {
+			link = "FloatBorder",
+		},
+		PopupNormal = {
+			-- bg = "None",
+			bg = bg,
+			fg = colors.text,
+		},
+		Pmenu = {
+			link = "FloatBorder",
+		},
+		PmenuBorder = {
+			link = "FloatBorder",
+		},
+		PmenuDocBorder = {
+			link = "FloatBorder",
+		},
+		PmenuSel = {
+			-- link = "Visual",
+			bg = colors.surface0,
+		},
+		NeoTreeNormal = {
+			-- bg = "None",
+			bg = bg,
+		},
+		IlluminatedWordRead = {
+			bold = true,
+			bg = colors.surface1,
+		},
+		IlluminatedWordWrite = {
+			link = "IlluminatedWordRead",
+		},
+		IluminatedReferenceText = {
+			link = "IlluminatedWordRead",
+		},
+		CmpItemAbbrMatch = {
+			fg = colors.mauve,
+			-- bold = true,
+			-- underline = true,
+		},
+		CmpItemAbbrMatchFuzzy = {
+			-- fg = colors.blue,
+			-- underline = true,
+			link = "CmpItemAbbrMatch",
+		},
+		TelescopeSelection = {
+			link = "PmenuSel",
+		},
+		FlashLabel = {
+			fg = "#000000",
+			bg = "#FFFF00",
+			bold = true,
+		},
+		NeoTreeIndentMarker = {
+			fg = colors.surface0,
+		},
+		NeoTreeCursorLine = {
+			link = "PmenuSel",
+		},
+		NoicePopupmenu = {
+			link = "PopupNormal",
+		},
+		NoicePopupmenuBorder = {
+			link = "FloatBorder",
+		},
+		MiniIndentscopeSymbol = {
+			fg = colors.surface0,
+		},
+		LspSignatureActiveParameter = {
+			bg = colors.peach,
+			fg = colors.crust,
+		},
+		GitSignsChange = {
+			fg = colors.blue,
+		},
+		GitSignsAdd = {
+			fg = colors.green,
+		},
+		SwitchBufferStatusColor = {
+			fg = colors.red,
+		},
+		CmpGhostText = { link = "Comment", default = true },
+		-- StatusLine = {
+		--     bg = colors.mantle,
+		-- },
+		--
+		-- StatusLineNC = {
+		--     link = "StatusLine",
+		-- },
 
-        TelescopeMatching = { link = "CmpItemAbbrMatch" },
+		TelescopeMatching = { link = "CmpItemAbbrMatch" },
 
-        -- TelescopePromptPrefix = { bg = colors.surface0 },
-        -- TelescopePromptNormal = { bg = colors.surface0 },
-        -- TelescopePromptBorder = { bg = colors.surface0, fg = colors.surface0 },
-        TelescopePromptTitle = { bg = colors.pink, fg = colors.base },
+		-- TelescopePromptPrefix = { bg = colors.surface0 },
+		-- TelescopePromptNormal = { bg = colors.surface0 },
+		-- TelescopePromptBorder = { bg = colors.surface0, fg = colors.surface0 },
+		TelescopePromptTitle = { bg = colors.pink, fg = colors.base },
 
-        TelescopeResultsNormal = { fg = colors.subtext1 },
-        -- TelescopeResultsBorder = { bg = colors.mantle, fg = colors.mantle },
-        TelescopeResultsTitle = { bg = colors.blue, fg = colors.base },
+		TelescopeResultsNormal = { fg = colors.subtext1 },
+		-- TelescopeResultsBorder = { bg = colors.mantle, fg = colors.mantle },
+		TelescopeResultsTitle = { bg = colors.blue, fg = colors.base },
 
-        -- TelescopePreviewBorder = { bg = colors.base, fg = colors.base },
-        TelescopePreviewNormal = {},
-        TelescopePreviewTitle = { bg = colors.green, fg = colors.base },
-    }
+		-- TelescopePreviewBorder = { bg = colors.base, fg = colors.base },
+		TelescopePreviewNormal = {},
+		TelescopePreviewTitle = { bg = colors.green, fg = colors.base },
+	}
 end
 
 function M.override_hl(colors_table)
-    local colors = M.get(colors_table)
+	local colors = M.get(colors_table)
 
-    for hl, col in pairs(colors) do
-        vim.api.nvim_set_hl(0, hl, col)
-    end
+	for hl, col in pairs(colors) do
+		vim.api.nvim_set_hl(0, hl, col)
+	end
 end
 
 function M.override_lsp_hl(colors_table)
-    local colors = colors_table
+	local colors = colors_table
 
-    vim.api.nvim_set_hl(0, "@parameter", { fg = colors.red, italic = true })
-    vim.api.nvim_set_hl(0, "@variable", { fg = colors.text, italic = false })
-    vim.api.nvim_set_hl(0, "@operator", { fg = colors.teal, italic = false })
-    vim.api.nvim_set_hl(0, "@attribute", { fg = colors.lavender, italic = true })
-    vim.api.nvim_set_hl(0, "@keyword.operator", { link = "@repeat" })
+	vim.api.nvim_set_hl(0, "@parameter", { fg = colors.red, italic = true })
+	vim.api.nvim_set_hl(0, "@variable", { fg = colors.text, italic = false })
+	vim.api.nvim_set_hl(0, "@operator", { fg = colors.teal, italic = false })
+	vim.api.nvim_set_hl(0, "@attribute", { fg = colors.lavender, italic = true })
+	vim.api.nvim_set_hl(0, "@keyword.operator", { link = "@repeat" })
 
-    vim.api.nvim_set_hl(0, "@type.builtin", { fg = colors.red })
-    vim.api.nvim_set_hl(0, "@type.qualifier", { fg = colors.red })
-    vim.api.nvim_set_hl(0, "@storageclass", { fg = colors.red })
-    vim.api.nvim_set_hl(0, "@field", { fg = colors.red })
+	vim.api.nvim_set_hl(0, "@type.builtin", { fg = colors.red })
+	vim.api.nvim_set_hl(0, "@type.qualifier", { fg = colors.red })
+	vim.api.nvim_set_hl(0, "@storageclass", { fg = colors.red })
+	vim.api.nvim_set_hl(0, "@field", { fg = colors.red })
 
-    vim.api.nvim_set_hl(0, "@property", { fg = colors.text })
-    vim.api.nvim_set_hl(0, "@field", { fg = colors.text })
+	vim.api.nvim_set_hl(0, "@property", { fg = colors.text })
+	vim.api.nvim_set_hl(0, "@field", { fg = colors.text })
 
-    vim.api.nvim_set_hl(0, "@variable.builtin", { fg = colors.red })
+	vim.api.nvim_set_hl(0, "@variable.builtin", { fg = colors.red })
 
-    -- vim.api.nvim_set_hl(0, "@variable", { fg = colors.text })
-    vim.api.nvim_set_hl(0, "@_parent", { fg = colors.peach })
+	-- vim.api.nvim_set_hl(0, "@variable", { fg = colors.text })
+	vim.api.nvim_set_hl(0, "@_parent", { fg = colors.peach })
 end
 
 return M
