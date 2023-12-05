041556d807c3875b9ab72cc3b721e0d8fd9b7e72
Raphaël CHARTIER <raphaelchartier@free.fr>
2023-12-05 15:27:42 +0100

    nvim: change messages lsp lens

--- .config/nvim/lua/plugins/lsp/lens.lua
+++ .config/nvim/lua/plugins/lsp/lens.lua
@@ -3,7 +3,22 @@ local M = {
 }
 
 function M.config()
-	require("lsp-lens").setup({})
+	require("lsp-lens").setup({
+		sections = {
+			definition = function(count)
+				return "Def: " .. count
+			end,
+			references = function(count)
+				return "Ref: " .. count
+			end,
+			implements = function(count)
+				return "Imp: " .. count
+			end,
+			git_authors = function(latest_author, count)
+				return "  " .. latest_author .. (count - 1 == 0 and "" or (" + " .. count - 1))
+			end,
+		},
+	})
 end
 
 return M
