5d077ca2507232fbeb55c016bbbd4eb9176bf42f
Raphaël CHARTIER <raphaelchartier@free.fr>
2024-01-21 14:06:07 +0100

    nvim: readd gp

--- .config/nvim/lua/plugins/ia/gpt.lua
+++ .config/nvim/lua/plugins/ia/gpt.lua
@@ -1,13 +1,11 @@
 local M = {
 	"robitx/gp.nvim",
-	keys = {
-		{ "<C-g>t", "<cmd>GpChatToggle popup<cr>" },
-	},
-	enabled = false,
+	enabled = true,
 }
 
 function M.config()
 	require("gp").setup()
+	vim.keymap.set("n", "<C-g>t", "<cmd>GpChatToggle popup<cr>", { desc = "Toggle Gpt Popup" })
 end
 
 return M
