557fd27710b2ced9db293bd28278106a65a99d99
Raphaël CHARTIER <raphaelchartier@free.fr>
2024-02-04 22:44:11 +0100

    nvim: better markdown icons

--- .config/nvim/queries/markdown/highlights.scm
+++ .config/nvim/queries/markdown/highlights.scm
@@ -15,8 +15,8 @@
 ; )
 
 ; checkboxes
-((task_list_marker_unchecked) @markdown_check_undone (#set! conceal "☐"))
-((task_list_marker_checked) @markdown_check_done (#set! conceal "✔"))
+((task_list_marker_unchecked) @markdown_check_undone (#set! conceal "󰄱"))
+((task_list_marker_checked) @markdown_check_done (#set! conceal "󰄲"))
 
 ; box drawing characters for tables
 (pipe_table_header ("|") @punctuation.special @conceal (#set! conceal "│"))
