e424e89d0f8b69394b5767aa8774d53b65791aa0
Raphaël CHARTIER <raphaelchartier@free.fr>
2024-02-12 10:54:54 +0100

    nvim: fix omnisharp

--- .config/nvim/lua/config/lsp/attach.lua
+++ .config/nvim/lua/config/lsp/attach.lua
@@ -78,7 +78,7 @@ M.on_attach = function(client, bufnr)
 
 	if client.name == "omnisharp" then
 		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts, { desc = "Open code action menu" })
-		vim.keymap.set("n", "gd", vim.lsp.buf.code_action, bufopts)
+		vim.keymap.set("n", "gd", require("omnisharp_extended").lsp_definitions, bufopts)
 	else
 		vim.keymap.set(
 			{ "v", "n" },
--- .config/nvim/lua/plugins/lsp/nullls.lua
+++ .config/nvim/lua/plugins/lsp/nullls.lua
@@ -1,71 +1,71 @@
 local M = {
-    "nvimtools/none-ls.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    enabled = false,
+	"nvimtools/none-ls.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	enabled = false,
 }
 
 local with_diagnostics_code = function(builtin)
-    return builtin.with({
-        diagnostics_format = "#{m} [#{c}]",
-    })
+	return builtin.with({
+		diagnostics_format = "#{m} [#{c}]",
+	})
 end
 
 local with_root_file = function(builtin, file)
-    return builtin.with({
-        condition = function(utils)
-            return utils.root_has_file(file)
-        end,
-    })
+	return builtin.with({
+		condition = function(utils)
+			return utils.root_has_file(file)
+		end,
+	})
 end
 
 function M.config()
-    local U = require("utils")
-    local null_ls = require("null-ls")
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+	local U = require("utils")
+	local null_ls = require("null-ls")
+	local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-    null_ls.setup({
-        border = U.default_border,
-        on_attach = function(client, bufnr)
-            if client.supports_method("textDocument/formatting") then
-                vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-                vim.api.nvim_create_autocmd("BufWritePre", {
-                    group = augroup,
-                    buffer = bufnr,
-                    callback = function()
-                        vim.lsp.buf.format({ async = false })
-                    end,
-                })
-            end
-        end,
-        sources = {
-            null_ls.builtins.formatting.stylua,
-            -- null_ls.builtins.diagnostics.eslint,
-            null_ls.builtins.completion.spell,
-            null_ls.builtins.formatting.prettierd.with({
-                disabled_filetypes = { "markdown" },
-            }),
-            -- null_ls.builtins.formatting.fixjson,
-            null_ls.builtins.formatting.black.with({ extra_args = { "--line-length=120" } }),
-            null_ls.builtins.formatting.ruff,
-            null_ls.builtins.formatting.isort,
+	null_ls.setup({
+		border = U.default_border,
+		on_attach = function(client, bufnr)
+			if client.supports_method("textDocument/formatting") then
+				vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					group = augroup,
+					buffer = bufnr,
+					callback = function()
+						vim.lsp.buf.format({ async = false })
+					end,
+				})
+			end
+		end,
+		sources = {
+			null_ls.builtins.formatting.stylua,
+			-- null_ls.builtins.diagnostics.eslint,
+			null_ls.builtins.completion.spell,
+			null_ls.builtins.formatting.prettierd.with({
+				disabled_filetypes = { "markdown" },
+			}),
+			-- null_ls.builtins.formatting.fixjson,
+			null_ls.builtins.formatting.black.with({ extra_args = { "--line-length=120" } }),
+			null_ls.builtins.formatting.ruff,
+			null_ls.builtins.formatting.isort,
 
-            null_ls.builtins.formatting.clang_format.with({
-                extra_args = { '--style="{BasedOnStyle: llvm, IndentWidth: 16}"' },
-            }),
+			null_ls.builtins.formatting.clang_format.with({
+				extra_args = { '--style="{BasedOnStyle: llvm, IndentWidth: 16}"' },
+			}),
 
-            -- null_ls.builtins.formatting.csharpier,
+			-- null_ls.builtins.formatting.csharpier,
 
-            -- code actions
-            null_ls.builtins.code_actions.refactoring,
-            null_ls.builtins.code_actions.gitsigns,
+			-- code actions
+			null_ls.builtins.code_actions.refactoring,
+			null_ls.builtins.code_actions.gitsigns,
 
-            -- hover
-            null_ls.builtins.hover.dictionary,
-            null_ls.builtins.hover.printenv,
+			-- hover
+			null_ls.builtins.hover.dictionary,
+			null_ls.builtins.hover.printenv,
 
-            null_ls.builtins.completion.spell,
-        },
-    })
+			null_ls.builtins.completion.spell,
+		},
+	})
 end
 
 return M
