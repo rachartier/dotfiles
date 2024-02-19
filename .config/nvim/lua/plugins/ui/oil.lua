3ec0cc943bc2325d6674ca5f58282b28bdb21d9a
Raphaël CHARTIER <raphaelchartier@free.fr>
2024-02-19 21:54:34 +0100

    nvim: add oil

--- .config/nvim/lua/plugins/ui/oil.lua
+++ .config/nvim/lua/plugins/ui/oil.lua
@@ -1,18 +1,41 @@
 local M = {
-    "stevearc/oil.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    enabled = false,
+	"stevearc/oil.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	enabled = true,
 }
 
 function M.config()
-    require("oil").setup({
-        keymaps = {
-            ["<BS>"] = "actions.parent",
-        },
-    })
-    vim.keymap.set("n", "<leader>te", function()
-        require("oil").open_float()
-    end, { desc = "Open parent directory" })
+	require("oil").setup({
+		keymaps = {
+			["<BS>"] = "actions.parent",
+		},
+		float = {
+			padding = 2,
+			max_width = 0,
+			max_height = 0,
+			border = "rounded",
+			win_options = {
+				winblend = 0,
+			},
+			override = function(conf)
+				local win_height = math.ceil(vim.o.lines * 0.3)
+				local win_width = math.ceil(vim.o.columns * 0.4)
+
+				local row = math.ceil((vim.o.lines - win_height) * 0.4)
+				local col = math.ceil((vim.o.columns - win_width) * 0.5)
+
+				conf.width = win_width
+				conf.height = win_height
+				conf.row = row
+				conf.col = col
+
+				return conf
+			end,
+		},
+	})
+	vim.keymap.set("n", "<leader>to", function()
+		require("oil").open_float()
+	end, { desc = "Open parent directory" })
 end
 
 return M
