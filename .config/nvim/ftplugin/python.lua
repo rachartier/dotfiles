4e0065c8e9db679d278d79b2226b600ca01e4669
Raphaël CHARTIER <raphaelchartier@free.fr>
2024-04-23 17:55:47 +0200

    nvim: fix python

--- .config/nvim/ftplugin/python.lua
+++ .config/nvim/ftplugin/python.lua
@@ -1,9 +1,12 @@
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
+vim.opt_local.expandtab = true
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
+vim.opt_local.softtabstop = 4
 
 -- vim.opt_local.foldmethod = "indent"
 
 vim.cmd.inoreabbrev("<buffer> true True")
 vim.cmd.inoreabbrev("<buffer> false False")
+vim.cmd.inoreabbrev("<buffer> null None")
+vim.cmd.inoreabbrev("<buffer> none None")
+vim.cmd.inoreabbrev("<buffer> nil None")
