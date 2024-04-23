fddec253f100a175f4d15084c910db8b17f40eb0
Raphaël CHARTIER <raphaelchartier@free.fr>
2024-04-23 19:47:18 +0200

    dot: fix fzf tmuxp

--- .config/scripts/fzf_tmuxp.sh
+++ .config/scripts/fzf_tmuxp.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
 projects="$(tmuxp ls)"
-selected_project=$(echo "$projects" | fzf --reverse --height 100% --preview '')
-
-[[ -z "$selected_project" ]] && exit
-
-tmuxp load "$selected_project" -y
+echo "$projects" | fzf --reverse --height 100% --preview '' --preview-window down:50% --bind 'enter:execute(tmuxp load {} -y)'
