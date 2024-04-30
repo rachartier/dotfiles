a1ade99cec10d1b5d9b3072fca1159fca9dd9690
Raphaël CHARTIER <raphaelchartier@free.fr>
2024-04-30 09:07:52 +0200

    nvim: add markdown previewer

--- .config/nvim/lua/plugins/notes/markdown.lua
+++ .config/nvim/lua/plugins/notes/markdown.lua
@@ -44,6 +44,18 @@ return {
 		enabled = true,
 	},
 	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+		enabled = true,
+	},
+	{
 		"ellisonleao/glow.nvim",
 		ft = "markdown",
 		config = function()
