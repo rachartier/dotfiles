64b0e51e96ad8454b9ec31496bdb49f08090f0d6
Raphaël CHARTIER <raphaelchartier@free.fr>
2024-05-12 20:02:13 +0200

    nvim: fix copilot icon

--- .config/nvim/lua/plugins/ui/lualine.lua
+++ .config/nvim/lua/plugins/ui/lualine.lua
@@ -303,10 +303,10 @@ return {
 				local ok, copilot_enabled = pcall(vim.api.nvim_buf_get_var, 0, "copilot_enabled")
 
 				if copilot_enabled then
-					return icons.signs.others.copilot
+					return icons.signs.others.copilot .. "  "
 				end
 
-				return icons.signs.others.copilot_disabled
+				return icons.signs.others.copilot_disabled .. "  "
 			end,
 			cond = cond_disable_by_ft,
 			color = { fg = colors.fg },
