98a946a618c503fee14a9a13590981faf4ee5c35
Raphaël CHARTIER <raphaelchartier@free.fr>
2024-06-25 15:42:02 +0200

    nvim: fix todocomments

--- .config/nvim/lua/plugins/ui/todocomments.lua
+++ .config/nvim/lua/plugins/ui/todocomments.lua
@@ -1,6 +1,7 @@
 return {
 	"folke/todo-comments.nvim",
 	dependencies = { "nvim-lua/plenary.nvim" },
+	event = "LazyFile",
 	cmd = { "TodoTrouble", "TodoTelescope" },
 	keys = {
 		{ "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Toggle TODO list" } },
