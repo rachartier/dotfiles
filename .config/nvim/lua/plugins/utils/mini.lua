51e1f4e7a6d4089ae6eab7a89026f5eea56f8fe2
Raphaël CHARTIER <raphaelchartier@free.fr>
2024-09-02 01:52:08 +0200

    nvim: add mini hipatterns

--- .config/nvim/lua/plugins/utils/mini.lua
+++ .config/nvim/lua/plugins/utils/mini.lua
@@ -29,7 +29,7 @@ return {
 	},
 	{
 		"echasnovski/mini.hipatterns",
-		enabled = false,
+		enabled = true,
 		event = { "VeryLazy" },
 		config = function()
 			local hipatterns = require("mini.hipatterns")
@@ -38,50 +38,6 @@ return {
 				highlighters = {
 					-- Highlight hex color strings (`#rrggbb`) using that color
 					hex_color = hipatterns.gen_highlighter.hex_color(),
-					hl_group = {
-						pattern = function(buf)
-							return vim.api.nvim_buf_get_name(buf):find("lua/" .. M.module)
-								and '^%s*%[?"?()[%w%.@]+()"?%]?%s*='
-						end,
-						group = function(buf, match)
-							local group = M.hl_group(match, buf)
-							print(group)
-							if group then
-								if M.cache[group] == nil then
-									M.cache[group] = false
-									local hl = vim.api.nvim_get_hl(0, { name = group, link = false, create = false })
-									if not vim.tbl_isempty(hl) then
-										hl.fg = hl.fg or vim.api.nvim_get_hl(0, { name = "Normal", link = false }).fg
-										M.cache[group] = true
-										vim.api.nvim_set_hl(0, group .. "Dev", hl)
-									end
-								end
-								return M.cache[group] and group .. "Dev" or nil
-							end
-						end,
-						extmark_opts = { priority = 2000 },
-					},
-
-					hl_color = {
-						pattern = {
-							"%f[%w]()c%.[%w_%.]+()%f[%W]",
-							"%f[%w]()colors%.[%w_%.]+()%f[%W]",
-							"%f[%w]()vim%.g%.terminal_color_%d+()%f[%W]",
-						},
-						group = function(_, match)
-							local parts = vim.split(match, ".", { plain = true })
-							local color = vim.tbl_get(M.globals, unpack(parts))
-							return type(color) == "string"
-								and require("mini.hipatterns").compute_hex_color_group(color, "fg")
-						end,
-						extmark_opts = function(_, _, data)
-							return {
-								virt_text = { { "⬤ ", data.hl_group } },
-								virt_text_pos = "inline",
-								priority = 2000,
-							}
-						end,
-					},
 				},
 			})
 		end,
--- .config/nvim/lua/plugins/utils/nvim-highlight-colors.lua
+++ .config/nvim/lua/plugins/utils/nvim-highlight-colors.lua
@@ -1,7 +1,7 @@
 return {
 	"brenoprata10/nvim-highlight-colors",
 	event = "VeryLazy",
-	enabled = true,
+	enabled = false,
 	opts = {
 		render = "background",
 		virtual_symbol_position = "inline",
