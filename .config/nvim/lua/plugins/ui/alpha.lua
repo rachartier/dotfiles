48c1194ea5d34c046a8397cfcbf77cdbd19eb021
Raphaël CHARTIER <raphaelchartier@free.fr>
2024-09-05 13:01:15 +0200

    nvim: removed lazy event for alpha

--- .config/nvim/lua/plugins/ui/alpha.lua
+++ .config/nvim/lua/plugins/ui/alpha.lua
@@ -41,7 +41,7 @@ end
 return {
 	"goolord/alpha-nvim",
 	init = false,
-	event = "VimEnter",
+	lazy = false,
 	opts = function()
 		local dashboard = require("alpha.themes.dashboard")
 
@@ -242,20 +242,20 @@ return {
 			footer = dashboard.section.footer,
 		})
 
-		utils.on_event("VimResized", function()
-			if vim.bo.filetype == "alpha" then
-				header_padding = get_heading_padding()
-
-				dashboard.opts.layout = build_layout({
-					header_padding = header_padding,
-					header = header,
-					buttons = dashboard.section.buttons,
-					footer = dashboard.section.footer,
-				})
-
-				-- pcall(vim.cmd.AlphaRedraw)
-			end
-		end, { desc = "Redraw alpha on resize" })
+		-- utils.on_event("VimResized", function()
+		-- 	if vim.bo.filetype == "alpha" then
+		-- 		header_padding = get_heading_padding()
+		--
+		-- 		dashboard.opts.layout = build_layout({
+		-- 			header_padding = header_padding,
+		-- 			header = header,
+		-- 			buttons = dashboard.section.buttons,
+		-- 			footer = dashboard.section.footer,
+		-- 		})
+		--
+		-- 		-- pcall(vim.cmd.AlphaRedraw)
+		-- 	end
+		-- end, { desc = "Redraw alpha on resize" })
 		-- no Idea how it works exactly, try n error with distinguishable colors lol
 		return dashboard
 	end,
