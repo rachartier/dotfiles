b847e3d3bab0284b465646ab059c5840de66b8e9
Raphaël CHARTIER <raphaelchartier@free.fr>
2024-09-25 01:22:34 +0200

    nvim: add trouble and multicursors

--- .config/nvim/lua/plugins/editing/multicursors.lua
+++ .config/nvim/lua/plugins/editing/multicursors.lua
@@ -1,15 +1,75 @@
 return {
-	"smoka7/multicursors.nvim",
-	cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
+	"jake-stewart/multicursor.nvim",
 	keys = {
 		{
-			mode = { "v", "n" },
-			"<Leader>m",
-			"<cmd>MCstart<cr>",
-			desc = "Create a selection for selected text or word under the cursor",
+			mode = { "n", "v" },
+			"<C-up>",
+			"<cmd>lua require('multicursor-nvim').addCursor('k')<CR>",
+		},
+		{
+			mode = { "n", "v" },
+			"<C-down>",
+			"<cmd>lua require('multicursor-nvim').addCursor('j')<CR>",
+		},
+		{
+			mode = { "n", "v" },
+			"<C-left>",
+			"<cmd>lua require('multicursor-nvim').addCursor('h')<CR>",
+		},
+		{
+			mode = { "n", "v" },
+			"<C-right>",
+			"<cmd>lua require('multicursor-nvim').addCursor('l')<CR>",
+		},
+		{
+			mode = { "n", "v" },
+			"<leader>mn",
+			"<cmd>lua require('multicursor-nvim').nextCursor<CR>",
+		},
+		{
+			mode = { "n", "v" },
+			"<leader>mp",
+			"<cmd>lua require('multicursor-nvim').prevCursor<CR>",
+		},
+		{
+			mode = { "n", "v" },
+			"<leader>mm",
+			"<cmd>lua require('multicursor-nvim').addCursor('*')<CR>",
+			-- function()
+			-- 	local mc = require("multicursor-nvim")
+			--
+			-- 	mc.action(function(ctx)
+			-- 		local has_cursor = ctx:hasCursors()
+			--
+			-- 		if has_cursor then
+			-- 			mc.addCursor("*")
+			-- 		end
+			-- 	end)
+			-- 	return require("multicursor-nvim").is_active()
+			-- end,
+		},
+		{
+			mode = { "n", "v" },
+			"<esc>",
+			function()
+				local mc = require("multicursor-nvim")
+
+				if not mc.cursorsEnabled() then
+					mc.enableCursors()
+				elseif mc.hasCursors() then
+					mc.clearCursors()
+				else
+					-- Default <esc> handler.
+				end
+			end,
+		},
+		{
+			mode = { "n", "v" },
+			"<leader>ma",
+			"<cmd>lua require('multicursor-nvim').alignCurosrs<CR>",
 		},
 	},
-	opts = {
-		hint_config = false,
-	},
+	config = function(_, opts)
+		require("multicursor-nvim").setup(opts)
+	end,
 }
