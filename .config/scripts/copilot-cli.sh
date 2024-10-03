cde3424448cd5430f709ac19dfc2761751316f6e
Raphaël CHARTIER <raphaelchartier@free.fr>
2024-10-03 20:25:31 +0200

    feat(copilot-cli): resolve relative path issue in script

--- .config/scripts/copilot-cli.sh
+++ .config/scripts/copilot-cli.sh
@@ -2,6 +2,18 @@
 
 script_path="$HOME/.config/scripts/copilot-cli"
 
-source "$script_path"/.venv/bin/activate
-python3 "$script_path"/copilot-cli.py "$@"
+# Launch the script with the path argument with the real current path
+args=("$@")
+for i in "${!args[@]}"; do
+    if [ "${args[$i]}" == "--path" ]; then
+        args[i + 1]="$(pwd)/${args[$i + 1]}"
+        break
+    fi
+done
+
+cd "$script_path" || exit
+
+source ".venv/bin/activate"
+
+python3 copilot-cli.py "${args[@]}"
 deactivate
