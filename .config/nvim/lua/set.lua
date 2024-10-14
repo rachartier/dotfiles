1b59c400a242263d19340db0636167253b0f8a94
Raphaël CHARTIER <raphaelchartier@free.fr>
2024-10-14 09:22:47 +0200

    fix(nvim): switch clipboard program to win32yank

--- .config/nvim/lua/set.lua
+++ .config/nvim/lua/set.lua
@@ -27,37 +27,39 @@ vim.schedule(function()
 		--         cache_enabled = true
 		--     }
 		-- end
-		vim.g.clipboard = {
-			name = "WslClipboard",
-			copy = {
-				["+"] = "clip.exe",
-				["*"] = "clip.exe",
-			},
-			paste = {
-				["+"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-				["*"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-			},
-			cache_enabled = 0,
-		}
-
-		if os.getenv("TMUX") then
-			utils.on_event({ "TextYankPost" }, function()
-				vim.fn.system("clip.exe", vim.fn.getreg('"'))
-			end, { target = "*", desc = "Copy yanked text to clipboard" })
-		end
-
 		-- vim.g.clipboard = {
-		-- 	name = "win32yank-wsl",
+		-- 	name = "WslClipboard",
 		-- 	copy = {
-		-- 		["+"] = "win32yank.exe -i --crlf",
-		-- 		["*"] = "win32yank.exe -i --crlf",
+		-- 		["+"] = "clip.exe",
+		-- 		["*"] = "clip.exe",
 		-- 	},
 		-- 	paste = {
-		-- 		["+"] = "win32yank.exe -o --lf",
-		-- 		["*"] = "win32yank.exe -o --lf",
+		-- 		["+"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+		-- 		["*"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
 		-- 	},
 		-- 	cache_enabled = 0,
 		-- }
+		--
+		-- if os.getenv("TMUX") then
+		-- 	utils.on_event({ "TextYankPost" }, function()
+		-- 		vim.fn.system("clip.exe", vim.fn.getreg('"'))
+		-- 	end, { target = "*", desc = "Copy yanked text to clipboard" })
+		-- end
+
+		-- choco install win32yank
+		vim.g.clipboard = {
+			name = "win32yank-wsl",
+			copy = {
+				["+"] = "win32yank.exe -i --crlf",
+				["*"] = "win32yank.exe -i --crlf",
+			},
+			paste = {
+				["+"] = "win32yank.exe -o --lf",
+				["*"] = "win32yank.exe -o --lf",
+			},
+			cache_enabled = 0,
+		}
+		vim.opt.clipboard = "unnamedplus"
 	end
 end)
 
