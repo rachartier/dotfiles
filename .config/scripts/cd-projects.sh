4fa5281c6856355d7ced06910d0da5f3e16158ef
Raphaël Chartier <raphaelchartier@free.fr>
2025-03-24 21:38:02 +0100

    feat(scripts): add cd-projects.sh for project navigation

---
+++ .config/scripts/cd-projects.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+get_last_modified() {
+    local repo="$1"
+    if [ -d "$repo/.git" ]; then
+        # Get the latest commit date in YYYY-MM-DD format
+        git -C "$repo" log -1 --format="%cd" --date=short 2>/dev/null || echo "No commits"
+    else
+        echo "Not a git repo"
+    fi
+}
+
+repos_raw=$(fd -u -t d -H "^\.git$" "$HOME/dev" -x dirname {})
+repos=""
+while IFS= read -r repo; do
+    last_modified=$(get_last_modified "$repo")
+    name=$(echo "$repo" | awk -F/ '{print $(NF)}')
+    repos="${repos}${repo} :: [${last_modified}] ${name}"$'\n'
+done <<<"$repos_raw"
+
+repos=$(echo "$repos" | sort -k 3 -r)
+
+if [ -z "$repos" ]; then
+    echo "No projects found"
+    exit 1
+fi
+
+echo "$repos"
+
+selection=$(echo -e "$repos" | fzf --ansi --with-nth 3,4 --preview "eza --color=always --long --no-filesize --icons=always --no-time --no-user --no-permissions {1}")
+
+if [ -z "$selection" ]; then
+    exit 2
+fi
+
+project=$(echo "$selection" | awk -F" :: " '{print $1}')
+
+name=$(echo "$selection" | awk -F" :: " '{print $2}' | cut -d' ' -f2-)
+name=$(echo "$name" | tr . _)
+
+if [ -z "$project" ]; then
+    exit 3
+fi
+
+tmux new -s "$name" -d -c "$project" -n "$name"
+tmux switch-client -t "$name"
