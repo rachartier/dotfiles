dabf487892cda1fce543fd09d60c07f0284ef7e1
Raphaël CHARTIER <raphael.chartier@michelin.com>
2025-05-13 17:27:09 +0200

    feat(test): enhance neotest keymaps

--- .config/nvim/lua/plugins/utils/neotest.lua
+++ .config/nvim/lua/plugins/utils/neotest.lua
@@ -9,13 +9,73 @@ return {
 	},
 	lazy = true,
 	keys = {
-		{ "<leader>tn", "<cmd>lua require('neotest').run.run()<CR>", desc = "Run Neotest" },
-		{ "<leader>tf", "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<CR>", desc = "Run Neotest File" },
-		{ "<leader>ts", "<cmd>lua require('neotest').summary.toggle()<CR>", desc = "Toggle Neotest Summary" },
+		{
+			"<leader>t",
+			"",
+			desc = "+test",
+		},
+		{
+			"<leader>tr",
+			function()
+				require("neotest").run.run(vim.fn.expand("%"))
+			end,
+			desc = "Run File (Neotest)",
+		},
+		{
+			"<leader>tR",
+			function()
+				require("neotest").run.run(vim.uv.cwd())
+			end,
+			desc = "Run All Test Files (Neotest)",
+		},
+		{
+			"<leader>tr",
+			function()
+				require("neotest").run.run()
+			end,
+			desc = "Run Nearest (Neotest)",
+		},
+		{
+			"<leader>tl",
+			function()
+				require("neotest").run.run_last()
+			end,
+			desc = "Run Last (Neotest)",
+		},
+		{
+			"<leader>ts",
+			function()
+				require("neotest").summary.toggle()
+			end,
+			desc = "Toggle Summary (Neotest)",
+		},
 		{
 			"<leader>to",
-			"<cmd>lua require('neotest').output.open({ enter = true, auto_close = true })<CR>",
-			desc = "Open Neotest Output",
+			function()
+				require("neotest").output.open({ enter = true, auto_close = true })
+			end,
+			desc = "Show Output (Neotest)",
+		},
+		{
+			"<leader>tO",
+			function()
+				require("neotest").output_panel.toggle()
+			end,
+			desc = "Toggle Output Panel (Neotest)",
+		},
+		{
+			"<leader>tS",
+			function()
+				require("neotest").run.stop()
+			end,
+			desc = "Stop (Neotest)",
+		},
+		{
+			"<leader>tw",
+			function()
+				require("neotest").watch.toggle(vim.fn.expand("%"))
+			end,
+			desc = "Toggle Watch (Neotest)",
 		},
 	},
 	config = function()
@@ -25,6 +85,9 @@ return {
 					runner = "unittest",
 				}),
 			},
+			is_test_file = function(file_path)
+				return file_path:match("test_") or file_path:match("_test") or file_path:match("tests/")
+			end,
 		})
 	end,
 }
