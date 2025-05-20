3aa99a9f9c4a0576ecdc461aa706b79493a4045b
Raphaël Chartier <raphaelchartier@free.fr>
2025-05-20 21:34:11 +0200

    fix: correct dot-manager install script

--- devpod-install.sh
+++ devpod-install.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 export DOTFILES_DOCKER=1
 export GIT_CLONE_METHOD=https
-
-./"$HOME/.config/dot-manager/first_install.sh"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/rachartier/dotfiles/main/.config/dot-manager/first_install.sh)"
