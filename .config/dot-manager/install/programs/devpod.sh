37da1328bda8a9328f2d91e83e418bd3b47edb88
Raphaël Chartier <raphaelchartier@free.fr>
2025-05-20 22:57:16 +0200

    feat(devpod): set default dotfiles repo

--- .config/dot-manager/install/programs/devpod.sh
+++ .config/dot-manager/install/programs/devpod.sh
@@ -6,6 +6,10 @@ install_devpod() {
     log "info" "Installing devpod..."
 
     curl -L -o devpod "https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-amd64" && sudo install -c -m 0755 devpod /usr/local/bin && rm -f devpod
+    devpod context set-options -o DOTFILES_URL="https://github.com/rachartier/devpod-dotfiles"
+    log "info" "Devpod context set to use devpod-dotfiles repository"
+
+    log "success" "Devpod installed."
 }
 
 install_devpod "$@"
--- devpod-install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt update
-
-export DOTFILES_DOCKER=1
-export GIT_CLONE_METHOD=https
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/rachartier/dotfiles/main/.config/dot-manager/first_install.sh)"
