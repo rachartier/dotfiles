83062a3b76b2c3bb1680fc4801596b44ca316826
Raphaël Chartier <raphaelchartier@free.fr>
2025-05-27 00:29:54 +0200

    feat(treesitter): enable treesitter on filetype

--- .config/nvim/lua/plugins/utils/treesitter.lua
+++ .config/nvim/lua/plugins/utils/treesitter.lua
@@ -110,6 +110,22 @@ return {
 		init = function()
 			vim.g.loaded_nvim_treesitter = 1
 		end,
+		config = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				desc = "Enable Treesitter",
+				group = vim.api.nvim_create_augroup("enable_treesitter", {}),
+				-- Don't filter by `pattern`, install and enable Treesitter parsers for all languages.
+				callback = function()
+					-- Enable Treesitter syntax highlighting.
+					if pcall(vim.treesitter.start) then
+						-- Use Treesitter indentation and folds.
+						vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+						vim.wo.foldmethod = "expr"
+						vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+					end
+				end,
+			})
+		end,
 	},
 	{
 		"lewis6991/ts-install.nvim",
