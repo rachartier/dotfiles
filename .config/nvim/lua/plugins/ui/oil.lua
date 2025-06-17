c63d17877ef26928fc1035469a7845166175795c
Raphaël Chartier <raphaelchartier@free.fr>
2025-06-17 23:07:04 +0200

    feat(ui): re add oil.nvim

---
+++ .config/nvim/lua/plugins/ui/oil.lua
@@ -0,0 +1,40 @@
+return {
+	{
+		"stevearc/oil.nvim",
+		enabled = true,
+		keys = {
+			{
+				"<leader>te",
+				"<Cmd>Oil --float<CR>",
+				desc = "Open Oil",
+			},
+		},
+		opts = {
+			skip_confirm_for_simple_edits = true,
+			delete_to_trash = true,
+			cleanup_delay_ms = false,
+			watch_for_changes = true,
+			columns = {
+				"icon",
+				-- "permissions",
+				-- "size",
+				-- "mtime",
+			},
+			float = {
+				get_win_title = nil,
+				preview_split = "right",
+				border = require("config.ui.border").default_border,
+
+				max_width = 0.40,
+				max_height = 0.80,
+
+				override = function(conf)
+					return conf
+				end,
+			},
+			keymaps = {
+				["<BS>"] = { "actions.parent", mode = "n" },
+			},
+		},
+	},
+}
---
+++ .config/nvim/lua/themes/nano/theme.lua
@@ -0,0 +1,38 @@
+local M = {}
+
+function M.setup()
+	-- require("themes.groups").override_hl(M.get_colors())
+end
+
+function M.get_colors()
+	return {
+		base = "#1e1e2e",
+		mantle = "#181825",
+		crust = "#11111b",
+		text = "#cdd6f4",
+		subtext1 = "#bac2de",
+		subtext0 = "#a6adc8",
+		overlay2 = "#9399b2",
+		overlay1 = "#7f849c",
+		overlay0 = "#6c7086",
+		surface2 = "#585b70",
+		surface1 = "#45475a",
+		surface0 = "#313244",
+		blue = "#89b4fa",
+		lavender = "#b4befe",
+		sapphire = "#74c7ec",
+		sky = "#89dceb",
+		teal = "#94e2d5",
+		green = "#a6e3a1",
+		yellow = "#f9e2af",
+		peach = "#fab387",
+		maroon = "#eba0ac",
+		red = "#f38ba8",
+		mauve = "#cba6f7",
+		pink = "#f5c2e7",
+		flamingo = "#f2cdcd",
+		rosewater = "#f5e0dc",
+	}
+end
+
+return M
