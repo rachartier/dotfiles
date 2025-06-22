f814f4b89f30a13341ef2f833556e97c9f8122eb
Raphaël Chartier <raphaelchartier@free.fr>
2025-06-22 02:53:39 +0200

    feat(mini): add mini.ai

--- .config/nvim/lua/plugins/utils/mini.lua
+++ .config/nvim/lua/plugins/utils/mini.lua
@@ -132,6 +132,35 @@ return {
       vim.api.nvim_set_hl(0, "MiniCursorword", { link = "Visual" })
     end,
   },
+  "echasnovski/mini.ai",
+  event = "VeryLazy",
+  opts = function()
+    local ai = require("mini.ai")
+    return {
+      n_lines = 500,
+      custom_textobjects = {
+        o = ai.gen_spec.treesitter({ -- code block
+          a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+          i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+        }),
+        f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
+        c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
+        t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
+        d = { "%f[%d]%d+" }, -- digits
+        e = { -- Word with case
+          {
+            "%u[%l%d]+%f[^%l%d]",
+            "%f[%S][%l%d]+%f[^%l%d]",
+            "%f[%P][%l%d]+%f[^%l%d]",
+            "^[%l%d]+%f[^%l%d]",
+          },
+          "^().*()$",
+        },
+        u = ai.gen_spec.function_call(), -- u for "Usage"
+        U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }), -- without dot in function name
+      },
+    }
+  end,
   -- {
   -- 	"echasnovski/mini.animate",
   -- 	event = { "VeryLazy" },
