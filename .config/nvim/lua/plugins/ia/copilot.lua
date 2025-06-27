cf9e03988dfc674d8dbfd6100a2fd37d7c455a68
Raphaël CHARTIER <raphael.chartier@michelin.com>
2025-06-27 11:20:30 +0200

    feat(ia): add CodeCompanion event status notifications

--- .config/nvim/lua/plugins/ia/copilot.lua
+++ .config/nvim/lua/plugins/ia/copilot.lua
@@ -272,6 +272,46 @@ return {
         },
       },
     },
+    config = function(_, opts)
+      require("codecompanion").setup(opts)
+
+      local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+      local group = vim.api.nvim_create_augroup("CodeCompanionFidgetHooks", { clear = true })
+      vim.api.nvim_create_autocmd({ "User" }, {
+        pattern = "CodeCompanion*",
+        group = group,
+        callback = function(request)
+          if request.match == "CodeCompanionChatSubmitted" then
+            return
+          end
+
+          local msg
+
+          msg = "[CodeCompanion] " .. request.match:gsub("CodeCompanion", "")
+
+          vim.notify(msg, vim.log.levels.INFO, {
+            timeout = 1000,
+            keep = function()
+              return not vim
+                .iter({ "Finished", "Opened", "Hidden", "Closed", "Cleared", "Created" })
+                :fold(false, function(acc, cond)
+                  return acc or vim.endswith(request.match, cond)
+                end)
+            end,
+            id = "code_companion_status",
+            title = "Code Companion Status",
+            opts = function(notif)
+              notif.icon = ""
+              ---@diagnostic disable-next-line: undefined-field
+              notif.icon = spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+              if vim.endswith(request.match, "Finished") then
+                notif.icon = " "
+              end
+            end,
+          })
+        end,
+      })
+    end,
   },
   -- {
   --   -- dir = os.getenv("HOME") .. "/dev/nvim_plugins/avante.nvim",
