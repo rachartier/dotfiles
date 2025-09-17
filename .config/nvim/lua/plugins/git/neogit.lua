67cd54cbe198c4f7f111c84f4d60c8fba0d99ce5
Raphaël Chartier <raphaelchartier@free.fr>
2025-09-17 19:05:18 +0200

    feat(neogit): use newer way of adding git_services

--- .config/nvim/lua/plugins/git/neogit.lua
+++ .config/nvim/lua/plugins/git/neogit.lua
@@ -19,11 +19,11 @@ return {
       underline = true,
     },
     git_services = {
-      ["github.com"] = "https://github.com/${owner}/${repository}/compare/${branch_name}?expand=1",
-      ["bitbucket.org"] = "https://bitbucket.org/${owner}/${repository}/pull-requests/new?source=${branch_name}&t=1",
-      ["gitlab.com"] = "https://gitlab.com/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
-      ["michelin.gitlab.com"] = "https://michelin.gitlab.com/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
-      ["azure.com"] = "https://dev.azure.com/${owner}/_git/${repository}/pullrequestcreate?sourceRef=${branch_name}&targetRef=${target}",
+      ["michelin.gitlab.com"] = {
+        pull_request = "https://michelin.gitlab.com/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
+        commit = "https://michelin.gitlab.com/${owner}/${repository}/-/commit/${oid}",
+        tree = "https://michelin.gitlab.com/${owner}/${repository}/-/tree/${branch_name}?ref_type=heads",
+      },
     },
     signs = {
       section = { "", "" },
