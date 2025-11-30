d5a8b562e8f3b863d65ac7798fbd923a7abd5ef5
Raphaël Chartier <raphaelchartier@free.fr>
2025-11-30 03:20:26 +0100

    feat(markdown): add markdown-plus.nvim dependency

--- .config/nvim/lua/plugins/notes/markdown.lua
+++ .config/nvim/lua/plugins/notes/markdown.lua
@@ -4,6 +4,9 @@ return {
     enabled = true,
     ft = { "markdown", "quarto", "rmd", "Avante", "codecompanion" },
     lazy = true,
+    dependencies = {
+      "yousefhadder/markdown-plus.nvim",
+    },
     opts = function(_, opts)
       local presets = require("markview.presets")
 
