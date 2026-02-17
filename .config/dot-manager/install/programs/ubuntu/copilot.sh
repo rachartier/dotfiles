c4923bbccf28d5bd9a7c36dc66bf364e59c9b436
Raphaël Chartier <raphaelchartier@free.fr>
2026-02-17 22:23:36 +0100

    refactor(yazi): update install method to binary zip

--- .config/dot-manager/install/programs/ubuntu/copilot.sh
+++ .config/dot-manager/install/programs/ubuntu/copilot.sh
@@ -19,37 +19,6 @@ install_copilot() {
 
     log "info" "Installing copilot ${copilot_version} ..."
     __install_package_release "https://github.com/github/copilot-cli/releases/download/$copilot_version/copilot-linux-x64.tar.gz" "copilot"
-
-    log "info" "Downloading 'beads'"
-
-    local bd_version
-    bd_version=$(__get_latest_release "steveyegge/beads")
-
-    __install_package_release "https://github.com/steveyegge/beads/releases/latest/download/beads_${bd_version#v}_linux_amd64.tar.gz" bd
-
-    log "info" "Installing 'beads-mcp' tool"
-    if command -v beads-mcp &>/dev/null; then
-        log "info" "'beads-mcp' is already installed"
-        if command -v uv &>/dev/null; then
-            log "info" "Updating beads-mcp using uv"
-            uv tool update beads-mcp
-        elif command -v pipx &>/dev/null; then
-            log "info" "Updating beads-mcp using pipx"
-            pipx upgrade beads-mcp
-        fi
-        return
-    fi
-
-    if ! command -v uv &>/dev/null; then
-        log "info" "'uv' is not installed, testing with pipx"
-        if command -v pipx &>/dev/null; then
-            log "info" "Installing beads-mcp using pipx"
-            pipx install beads-mcp
-            log "info" "'uv' installed successfully"
-        fi
-    else
-        uv tool install beads-mcp
-    fi
 }
 
 install_copilot "$@"
--- .config/dot-manager/install/programs/ubuntu/yazi.sh
+++ .config/dot-manager/install/programs/ubuntu/yazi.sh
@@ -3,17 +3,19 @@
 source "$DOT_MANAGER_DIR/helper.sh"
 
 install_yazi() {
-    YAZI_VERSION=$(__get_latest_release "sxyazi/yazi")
+    print_step "Installing Yazi ..."
 
-    print_step "Installing Yazi ${YAZI_VERSION:latest} ..."
-
-    wget "https://github.com/sxyazi/yazi/releases/download/$YAZI_VERSION/yazi-x86_64-unknown-linux-musl.deb" -O /tmp/yazi.deb
-
-    if sudo dpkg -i --force-depends /tmp/yazi.deb; then
-        log "success" "Yazi installed."
-    else
-        log "error" "Yazi not installed."
+    if dpkg -l | grep yazi; then
+        sudo dpkg --remove --force-remove-reinstreq yazi
     fi
+
+    # Install the binary directly
+    curl -fsSL https://github.com/sxyazi/yazi/releases/latest/download/yazi-x86_64-unknown-linux-musl.zip -o /tmp/yazi.zip
+    unzip /tmp/yazi.zip -d /tmp/yazi
+    sudo mv /tmp/yazi/yazi-x86_64-unknown-linux-musl/yazi /usr/local/bin/
+    sudo mv /tmp/yazi/yazi-x86_64-unknown-linux-musl/ya /usr/local/bin/
+    sudo mv /tmp/yazi/yazi-x86_64-unknown-linux-musl/completions/_yazi /usr/share/zsh/site-functions/_yazi
+    sudo mv /tmp/yazi/yazi-x86_64-unknown-linux-musl/completions/_ya /usr/share/zsh/site-functions/_ya
 }
 
 install_yazi "$@"
