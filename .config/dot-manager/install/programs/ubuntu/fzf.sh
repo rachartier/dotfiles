a7ffcbfd2e9c3c5a977c4e33018caacb6509c938
Raphaël Chartier <raphaelchartier@free.fr>
2026-02-17 22:28:31 +0100

    fix(fzf): disable zsh integration during install

--- .config/dot-manager/install/programs/ubuntu/fzf.sh
+++ .config/dot-manager/install/programs/ubuntu/fzf.sh
@@ -8,7 +8,7 @@ install_fzf() {
     if [ -d "$HOME/.fzf/" ]; then
         cd ~/.fzf || return 0
         git pull --quiet
-        if yes | ./install; then
+        if yes | ./install --no-zsh; then
             log "success" "fzf updated."
         else
             log "error" "fzf not updated."
@@ -20,7 +20,7 @@ install_fzf() {
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf >/dev/null
 
     log "info" "Installing fzf..."
-    yes | ~/.fzf/install >/dev/null
+    yes | ~/.fzf/install --no-zsh >/dev/null
 }
 
 install_fzf "$@"
